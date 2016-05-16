a429c29e012eb9dba18e2faa232a2916d22987d9
adiraomj <adiraomj@outlook.com>
2016-05-17 00:58:44 +0530

    Added thumbnail slider widget form

--- widgets/th-thumb-slider/th-thumb-slider.php
+++ widgets/th-thumb-slider/th-thumb-slider.php
@@ -25,7 +25,43 @@ class Themovation_SO_WB_ThumbSlider_Widget extends SiteOrigin_Widget {
 			),
 
 			array(
-				// Fields go here
+				'slides' => array(
+					'type' => 'repeater',
+					'label' => __('Thumbnail Slides' , 'themovation-widgets'),
+					'item_name'  => __('Slide', 'themovation-widgets'),
+					'item_label' => array(
+						'selector'     => "[id*='title']",
+						'update_event' => 'change',
+						'value_method' => 'val'
+					),
+					'fields' => array(
+
+						'image' => array(
+							'type' => 'media',
+							'fallback' => false,
+							'label' => __('Image', 'themovation-widgets'),
+							'default'     => '',
+							'library' => 'image',
+						),
+
+						'title' => array(
+							'type' => 'text',
+							'label' => __('Title', 'themovation-widgets'),
+							'placeholder' => __('Enter title here', 'themovation-widgets'),
+						),
+
+						'small_text' => array(
+							'type' => 'text',
+							'label' => __('Small Text', 'themovation-widgets'),
+						),
+
+						'link' => array(
+							'type' => 'widget',
+							'class' => 'Themovation_SO_WB_Link_Widget',
+							'label' => __('Link', 'themovation-widgets'),
+						),
+					)
+				),
 			),
 
 			plugin_dir_path(__FILE__)
