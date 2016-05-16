e0e70d685a538139c8f836a85a1fcbc3121c729a
adiraomj <adiraomj@outlook.com>
2016-05-17 00:55:11 +0530

    Added testimonial widget form

--- widgets/th-testimonials/th-testimonials.php
+++ widgets/th-testimonials/th-testimonials.php
@@ -25,7 +25,45 @@ class Themovation_SO_WB_Testimonials_Widget extends SiteOrigin_Widget {
 			),
 
 			array(
-				// Fields go here
+				'testimonials' => array(
+					'type' => 'repeater',
+					'label' => __('Testimonials' , 'themovation-widgets'),
+					'item_name'  => __('Testimonial', 'themovation-widgets'),
+					'item_label' => array(
+						'selector'     => "[id*='title']",
+						'update_event' => 'change',
+						'value_method' => 'val'
+					),
+					'fields' => array(
+
+						'name' => array(
+							'type' => 'text',
+							'label' => __('Name', 'themovation-widgets'),
+							'placeholder' => __('John Doe', 'themovation-widgets'),
+						),
+
+						'title' => array(
+							'type' => 'text',
+							'label' => __('Title', 'themovation-widgets'),
+							'placeholder' => __('Satisfied Customer, CEO', 'themovation-widgets'),
+						),
+
+						'quote' => array(
+							'type' => 'text',
+							'label' => __('Quote', 'themovation-widgets'),
+							'placeholder' => __('Five Star Rating. Amazing Service.', 'themovation-widgets'),
+							'rows' => 4
+						),
+
+						'image' => array(
+							'type' => 'media',
+							'fallback' => false,
+							'label' => __('Image', 'themovation-widgets'),
+							'default'     => '',
+							'library' => 'image',
+						),
+					)
+				),
 			),
 
 			plugin_dir_path(__FILE__)
