f064d90ee3d59140292cc967a1109a38d13a3584
adiraomj <adiraomj@outlook.com>
2016-05-17 00:23:31 +0530

    Added header widget form

--- widgets/th-header/th-header.php
+++ widgets/th-header/th-header.php
@@ -25,7 +25,43 @@ class Themovation_SO_WB_Header_Widget extends SiteOrigin_Widget {
 			),
 
 			array(
-				// Fields go here
+				'icon' => array(
+					'type' => 'widget',
+					'class' => 'Themovation_SO_WB_Icon_Widget',
+					'label' => __('Icon', 'themovation-widgets'),
+				),
+
+				'title' => array(
+					'type' => 'text',
+					'label' => __('Title', 'themovation-widgets'),
+					'placeholder' => __('Enter title here', 'themovation-widgets'),
+				),
+
+				'content' => array(
+					'type' => 'tinymce',
+					'label' => __('Content', 'themovation-widgets'),
+					'default' => '',
+					'rows' => 10,
+					'default_editor' => 'tinymce',
+					'button_filters' => array(
+						'mce_buttons' => array( $this, 'filter_mce_buttons' ),
+						'mce_buttons_2' => array( $this, 'filter_mce_buttons_2' ),
+						'mce_buttons_3' => array( $this, 'filter_mce_buttons_3' ),
+						'mce_buttons_4' => array( $this, 'filter_mce_buttons_5' ),
+						'quicktags_settings' => array( $this, 'filter_quicktags_settings' ),
+					),
+				),
+
+				'align' => array(
+					'type' => 'select',
+					'label' => __('Align', 'themovation-widgets'),
+					'default' => 'left',
+					'options' => array(
+						'left' => __('Left', 'themovation-widgets'),
+						'centered' => __('Center', 'themovation-widgets'),
+						'right' => __('Right', 'themovation-widgets'),
+					)
+				),
 			),
 
 			plugin_dir_path(__FILE__)
