1fcbbd9444155e976a059a50ffb2ca049475eefa
adiraomj <adiraomj@outlook.com>
2016-05-17 00:26:25 +0530

    Added logos widget form

--- widgets/th-logos/th-logos.php
+++ widgets/th-logos/th-logos.php
@@ -25,7 +25,38 @@ class Themovation_SO_WB_Logos_Widget extends SiteOrigin_Widget {
 			),
 
 			array(
-				// Fields go here
+				'logos' => array(
+					'type' => 'repeater',
+					'label' => __('Logos' , 'themovation-widgets'),
+					'item_name'  => __('Item', 'themovation-widgets'),
+					'item_label' => array(
+						'selector'     => "[id*='title']",
+						'update_event' => 'change',
+						'value_method' => 'val'
+					),
+					'fields' => array(
+
+						'title' => array(
+							'type' => 'text',
+							'label' => __('Title', 'themovation-widgets'),
+							'placeholder' => __('Enter title here', 'themovation-widgets'),
+						),
+
+						'image' => array(
+							'type' => 'media',
+							'fallback' => false,
+							'label' => __('Image', 'themovation-widgets'),
+							'default'     => '',
+							'library' => 'image',
+						),
+
+						'link' => array(
+							'type' => 'widget',
+							'class' => 'Themovation_SO_WB_Link_Widget',
+							'label' => __('Link', 'themovation-widgets'),
+						),
+					)
+				)
 			),
 
 			plugin_dir_path(__FILE__)
