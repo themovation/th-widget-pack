7062050287d511838c5de3120c4fb57d68eb5b4f
adiraomj <adiraomj@outlook.com>
2016-05-16 23:51:38 +0530

    Added accordion widget form

--- widgets/th-accordions/th-accordions.php
+++ widgets/th-accordions/th-accordions.php
@@ -25,7 +25,58 @@ class Themovation_SO_WB_Accordions_Widget extends SiteOrigin_Widget {
 			),
 
 			array(
-				// Fields go here
+				'accordions' => array(
+					'type' => 'repeater',
+					'label' => __('Accordion' , 'themovation-widgets'),
+					'item_name'  => __('Collapsable Item', 'themovation-widgets'),
+					'item_label' => array(
+						'selector'     => "[id*='title']",
+						'update_event' => 'change',
+						'value_method' => 'val'
+					),
+					'fields' => array(
+
+						'title' => array(
+							'type' => 'text',
+							'label' => __('Title', 'themovation-widgets'),
+							'placeholder' => __('Enter title here', 'themovation-widgets'),
+						),
+
+						'content' => array(
+							'type' => 'tinymce',
+							'label' => __('Content', 'themovation-widgets'),
+							'default' => '',
+							'rows' => 10,
+							'default_editor' => 'tinymce',
+							'button_filters' => array(
+								'mce_buttons' => array( $this, 'filter_mce_buttons' ),
+								'mce_buttons_2' => array( $this, 'filter_mce_buttons_2' ),
+								'mce_buttons_3' => array( $this, 'filter_mce_buttons_3' ),
+								'mce_buttons_4' => array( $this, 'filter_mce_buttons_5' ),
+								'quicktags_settings' => array( $this, 'filter_quicktags_settings' ),
+							),
+						),
+
+						'button_1' => array(
+							'type' => 'widget',
+							'class' => 'Themovation_SO_WB_Button_Widget',
+							'label' => __('Button 1', 'themovation-widgets'),
+						),
+
+						'button_2' => array(
+							'type' => 'widget',
+							'class' => 'Themovation_SO_WB_Button_Widget',
+							'label' => __('Button 2', 'themovation-widgets'),
+						),
+
+						'icon' => array(
+							'type' => 'widget',
+							'class' => 'Themovation_SO_WB_Icon_Widget',
+							'label' => __('Icon', 'themovation-widgets'),
+						),
+
+					)
+				),
 			),
 
 			plugin_dir_path(__FILE__)
