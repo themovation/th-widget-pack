f8b9fc153091a27216aa04ab3312b34833ab79b2
adiraomj <adiraomj@outlook.com>
2016-05-17 01:08:21 +0530

    Added tour widget form

--- widgets/th-tour/th-tour.php
+++ widgets/th-tour/th-tour.php
@@ -25,7 +25,107 @@ class Themovation_SO_WB_Tour_Widget extends SiteOrigin_Widget {
 			),
 
 			array(
-				// Fields go here
+				'title' => array(
+					'type' => 'text',
+					'label' => __('Title', 'themovation-widgets'),
+					'placeholder' => __('Enter title here', 'themovation-widgets'),
+				),
+
+				'content' => array(
+					'type' => 'tinymce',
+					'label' => __('Content', 'themovation-widgets'),
+					'default' => '',
+					'rows' => 10,
+					'default_editor' => 'tinymce',
+					'button_filters' => array(
+						'mce_buttons' => array( $this, 'filter_mce_buttons' ),
+						'mce_buttons_2' => array( $this, 'filter_mce_buttons_2' ),
+						'mce_buttons_3' => array( $this, 'filter_mce_buttons_3' ),
+						'mce_buttons_4' => array( $this, 'filter_mce_buttons_5' ),
+						'quicktags_settings' => array( $this, 'filter_quicktags_settings' ),
+					),
+				),
+
+				'image' => array(
+					'type' => 'media',
+					'fallback' => false,
+					'label' => __('Image', 'themovation-widgets'),
+					'default'     => '',
+					'library' => 'image',
+				),
+
+				'button_1' => array(
+					'type' => 'widget',
+					'class' => 'Themovation_SO_WB_Button_Widget',
+					'label' => __('Button 1', 'themovation-widgets'),
+				),
+
+				'button_2' => array(
+					'type' => 'widget',
+					'class' => 'Themovation_SO_WB_Button_Widget',
+					'label' => __('Button 2', 'themovation-widgets'),
+				),
+
+				'styling' => array(
+					'type' => 'checkbox',
+					'default' => false,
+					'label' => __('Large Styling', 'themovation-widgets'),
+				),
+
+				'image_opts' => array(
+					'type' => 'section',
+					'label' => __('Featured Image Options' , 'themovation-widgets'),
+					'hide' => true,
+					'fields' => array(
+
+						'align' => array(
+							'type' => 'radio',
+							'label' => __('Align Photo', 'themovation-widgets'),
+							'default' => 'right',
+							'options' => array(
+								'left' => __('Left', 'themovation-widgets'),
+								'centered' => __('Center', 'themovation-widgets'),
+								'right' => __('Right', 'themovation-widgets'),
+							)
+						),
+
+						'size' => array(
+							'type'    => 'radio',
+							'default' => 'small',
+							'label'   => __('Photo Siz', 'themovation-widgets'),
+							'options' => array(
+								'small' => __('Small', 'themovation-widgets'),
+								'large' => __('Large', 'themovation-widgets'),
+							),
+						),
+
+						'pin' => array(
+							'type' => 'radio',
+							'label' => __('Pin Photo', 'themovation-widgets'),
+							'default' => 'none',
+							'state_emitter' => array(
+								'callback' => 'select',
+								'args' => array( 'photo_pin' )
+							),
+							'options' => array(
+								'top' => __('Sticky Top', 'themovation-widgets'),
+								'none' => __('None', 'themovation-widgets'),
+								'bottom' => __('Sticky Bottom', 'themovation-widgets'),
+							)
+						),
+
+						'animation' => array(
+							'type' => 'checkbox',
+							'default' => false,
+							'label' => __('Hover Slide Up Animation', 'themovation-widgets'),
+							'state_handler' => array(
+								'photo_pin[top]' => array('hide'),
+								'photo_pin[none]' => array('hide'),
+								'photo_pin[bottom]' => array('show'),
+							),
+						),
+					),
+				),
 			),
 
 			plugin_dir_path(__FILE__)
