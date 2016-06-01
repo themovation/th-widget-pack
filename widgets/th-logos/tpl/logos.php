bd7dddf0ea29f231e59a1aa9f6ea39e118f1818f
Teagan <teaganmckay@gmail.com>
2016-06-01 16:20:27 -0700

    Added initial markup and CSS for Logos widget

---
+++ widgets/th-logos/tpl/logos.php
@@ -0,0 +1,7 @@
+<div class="th-logos">
+    <img class="th-logo-img" src="http://placehold.it/150x150">
+    <img class="th-logo-img" src="http://placehold.it/150x150">
+    <img class="th-logo-img" src="http://placehold.it/150x150">
+    <img class="th-logo-img" src="http://placehold.it/150x150">
+    <img class="th-logo-img" src="http://placehold.it/150x150">
+</div>
