0020b37dbde3b2b4237b115fabcb40e916f837d2
Teagan <teaganmckay@gmail.com>
2016-06-01 15:54:42 -0700

    Added sample markup for CTA widget

---
+++ widgets/th-call-to-action/tpl/call-to-action.php
@@ -0,0 +1,9 @@
+<div class="th-cta">
+    <div class="th-cta-text">
+        <span>Is this theme a good fit for you? Buy it now on ThemeForest!</span>
+    </div>
+    <div class="th-cta-btn">
+        <a href="#" class="btn th-btn btn-standard">Download Theme</a>
+        <a href="#" class="btn th-btn btn-ghost">Download Theme</a>
+    </div>
+</div>
