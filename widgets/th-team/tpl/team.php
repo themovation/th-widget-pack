e6eda2d17597ec4e1e66b440c5d1044bd28f350a
Teagan <teaganmckay@gmail.com>
2016-06-01 16:21:58 -0700

    Added initial markup and CSS for Team widget

---
+++ widgets/th-team/tpl/team.php
@@ -0,0 +1,14 @@
+<div class="th-team-member">
+    <div class="th-team-member-wrap">
+        <img class="th-team-member-image" src="http://placehold.it/480x320">
+        <h4>Chris Anderson</h4>
+        <h5>Chief Product Officer</h5>
+        <div class="th-team-member-bio">
+            <p>Chris leads Product Management, Design, and Marketing functions globally. He is responsible for the core products and features.</p>
+        </div>
+        <div class="th-team-member-social">
+            <a target="_blank" href=""><i class="soc-icon social social-facebook"></i></a>
+            <a target="_blank" href=""><i class="soc-icon social social-twitter"></i></a>
+        </div>
+    </div>
+</div>
