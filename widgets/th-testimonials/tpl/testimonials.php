da6be9508bdab886889d6e8ca9dd56a1454ef7f4
Teagan <teaganmckay@gmail.com>
2016-06-01 16:22:09 -0700

    Added initial markup and CSS for Testimonials widget

---
+++ widgets/th-testimonials/tpl/testimonials.php
@@ -0,0 +1,9 @@
+<div class="th-testimonials">
+    <div class="th-testimonial-single">
+        <figure class="th-quote">
+            <blockquote>"Love this App. Proin venenatis, odio eget consectetur tincidunt, lacus magna."</blockquote>
+            <img width="60" height="60" src="http://placehold.it/60x60" class="th-circle" alt="" />
+            <figcaption>Kim Smith<span>Satisfied Customer</span></figcaption>
+        </figure>
+    </div>
+</div>
