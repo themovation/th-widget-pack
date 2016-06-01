4ade8e477c868bee42dd30d44726a295b4100446
Teagan <teaganmckay@gmail.com>
2016-06-01 16:21:21 -0700

    Added initial markup and CSS for Service blocks widget

---
+++ widgets/th-service/tpl/service.php
@@ -0,0 +1,31 @@
+<div class="th-service-blocks">
+    <div class="th-sb-single">
+        <div class="med-icon">
+            <i class="accent glyphicons glyphicons-cloud-upload"></i>
+        </div>
+        <div class="th-sb-text">
+            <h3>Standard Service Block</h3>
+            <p>Integer nec neque leo. Pellentesque nec ipsum ante. Vivamus viverra, libero et hendrerit placerat.</p>
+        </div>
+    </div>
+
+    <div class="th-sb-single-circle">
+        <div class="circle-med-icon">
+            <i class="accent glyphicons glyphicons-cloud-upload"></i>
+        </div>
+        <div class="th-sb-text">
+            <h3>Circle Style Service Block</h3>
+            <p>Integer nec neque leo. Pellentesque nec ipsum ante. Vivamus viverra, libero et hendrerit placerat.</p>
+        </div>
+    </div>
+
+    <div class="th-sb-single-vert">
+        <div class="circle-lrg-icon">
+            <i class="accent glyphicons glyphicons-cloud-upload"></i>
+        </div>
+        <div class="th-sb-text">
+            <h3>Circle Style Service Block</h3>
+            <p>Integer nec neque leo. Pellentesque nec ipsum ante. Vivamus viverra, libero et hendrerit placerat.</p>
+        </div>
+    </div>
+</div>
