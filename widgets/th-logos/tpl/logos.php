b7117ce71ffd6581cf68708516b0681ec63f06cf
Teagan <teaganmckay@gmail.com>
2016-06-03 15:36:55 -0700

    Changed sizing in example

--- widgets/th-logos/tpl/logos.php
+++ widgets/th-logos/tpl/logos.php
@@ -1,7 +1,7 @@
 <div class="th-logos">
-    <img class="th-logo-img" src="http://placehold.it/150x150">
-    <img class="th-logo-img" src="http://placehold.it/150x150">
-    <img class="th-logo-img" src="http://placehold.it/150x150">
-    <img class="th-logo-img" src="http://placehold.it/150x150">
-    <img class="th-logo-img" src="http://placehold.it/150x150">
+    <img class="th-logo-img" src="http://placehold.it/150x80" width="150" height="80">
+    <img class="th-logo-img" src="http://placehold.it/150x80" width="150" height="80">
+    <img class="th-logo-img" src="http://placehold.it/150x80" width="150" height="80">
+    <img class="th-logo-img" src="http://placehold.it/150x80" width="150" height="80">
+    <img class="th-logo-img" src="http://placehold.it/150x80" width="150" height="80">
 </div>
