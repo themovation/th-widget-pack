08307e15f4eaabbca474986fee8d7c282a7df3e2
Teagan <teaganmckay@gmail.com>
2016-06-03 15:38:41 -0700

    Added initial markup and CSS for button widget

---
+++ widgets/th-button/tpl/button.php
@@ -0,0 +1,27 @@
+<!-- Standard button example -->
+<a href="#" class="btn th-btn btn-standard">
+    Download
+    <i class="glyphicons glyphicons-cloud-upload"></i>
+</a>
+
+<!--
+
+<!-- Ghost button example ->
+<a href="#" class="btn th-btn btn-ghost">Download</a>
+
+<!-- CTA button example ->
+<a href="#" class="btn th-btn btn-cta">Download</a>
+
+<!-- Standard button example with icon ->
+<a href="#" class="btn th-btn btn-standard">
+    Download
+    <i class="glyphicons glyphicons-cloud-upload"></i>
+</a>
+
+<!-- Image button example ->
+<a href="#" target="_blank" title="Buy on Google Play" class="th-btn btn-image"><img src="http://placehold.it/170x60" alt=""></a>
+
+<!-- Add to Cart button example ->
+do_shortcode [add_to_cart id="123" sku=""]
+
+-->
