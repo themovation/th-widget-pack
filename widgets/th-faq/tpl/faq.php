beb1079b5b897bd5c690343c16b04b2c44341916
Teagan <teaganmckay@gmail.com>
2016-06-06 15:47:49 -0700

    Removed unique classes as likely not necessary

--- widgets/th-faq/tpl/faq.php
+++ widgets/th-faq/tpl/faq.php
@@ -1,9 +1,9 @@
 <div class="th-faq">
     <dl class="th-faq-list">
-        <dt class="th-faq-dt-0">What are the main differences between the regular license and the extended license?</dt>
-        <dd class="th-faq-dd-0">In simple terms, the main difference is that under the regular license, your end product (incorporating the item you've licensed) is distributed for free, whereas under the extended license your end product may be sold.</dd>
+        <dt class="th-faq-dt">What are the main differences between the regular license and the extended license?</dt>
+        <dd class="th-faq-dd">In simple terms, the main difference is that under the regular license, your end product (incorporating the item you've licensed) is distributed for free, whereas under the extended license your end product may be sold.</dd>
 
-        <dt class="th-faq-dt-1">What do you mean by item and end product?</dt>
-        <dd class="th-faq-dd-1">The item is what you purchase from the Envato marketplaces. The end product is what you build with that item.</dd>
+        <dt class="th-faq-dt">What do you mean by item and end product?</dt>
+        <dd class="th-faq-dd">The item is what you purchase from the Envato marketplaces. The end product is what you build with that item.</dd>
     </dl>
 </div>
