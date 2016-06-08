67a8062056844ec0d9f45e22a535892076c1dfe6
adiraomj <adiraomj@outlook.com>
2016-06-08 17:07:44 +0530

    Updated faq's template

--- widgets/th-faq/tpl/faq.php
+++ widgets/th-faq/tpl/faq.php
@@ -1,9 +1,12 @@
 <div class="th-faq">
     <dl class="th-faq-list">
-        <dt class="th-faq-dt">What are the main differences between the regular license and the extended license?</dt>
-        <dd class="th-faq-dd">In simple terms, the main difference is that under the regular license, your end product (incorporating the item you've licensed) is distributed for free, whereas under the extended license your end product may be sold.</dd>
 
-        <dt class="th-faq-dt">What do you mean by item and end product?</dt>
-        <dd class="th-faq-dd">The item is what you purchase from the Envato marketplaces. The end product is what you build with that item.</dd>
+		<?php foreach( $instance['faqs'] as $faq ) { ?>
+
+			<dt class="th-faq-dt"><?php echo $faq['title']; ?></dt>
+			<dd class="th-faq-dd"><?php echo $faq['content']; ?></dd>
+
+		<?php } ?>
+
     </dl>
 </div>
