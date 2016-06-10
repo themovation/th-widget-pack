16019e55206ffe3c73806e414a293e890e88713e
Teagan <teaganmckay@gmail.com>
2016-06-10 16:51:35 -0700

    Added initial icon examples

---
+++ widgets/th-icon/tpl/icon.php
@@ -0,0 +1,14 @@
+<!-- Standard icon example -->
+<span class="sow-icon-glyphicons" data-sow-icon="&#xe602;" ></span>
+
+<!-- Circle icon example -->
+<div class="circle-med-icon">
+    <span class="sow-icon-glyphicons" data-sow-icon="&#xe602;" ></span>
+</div>
+
+<!-- Bordered icon example -->
+<div class="circle-med-icon">
+    <span class="sow-icon-glyphicons" data-sow-icon="&#xe602;" ></span>
+</div>
+
+<!-- Graphic icon example -->
