45e4f5e31d0b45d818c0607da4697c4e5fb5768c
Teagan <teaganmckay@gmail.com>
2016-06-10 16:52:17 -0700

    Added initial link examples

---
+++ widgets/th-link/tpl/link.php
@@ -0,0 +1,5 @@
+<!-- Standard link example -->
+<a target="_blank" href="http://www.youtube.com">Link text here</a>
+
+<!-- Modal link example -->
+<a href="http://www.apple.com/" data-toggle="lightbox" data-width="1280">Link text here</a>
