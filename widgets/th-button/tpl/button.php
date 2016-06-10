8ae4d20ed649527b4615004bfe45245e2dff13a4
Teagan <teaganmckay@gmail.com>
2016-06-10 16:54:23 -0700

    Changed implementation of icons to match SO icon widget

--- widgets/th-button/tpl/button.php
+++ widgets/th-button/tpl/button.php
@@ -1,27 +1,21 @@
 <!-- Standard button example -->
-<a href="#" class="btn th-btn btn-standard">
-    Download
-    <i class="glyphicons glyphicons-cloud-upload"></i>
-</a>
+<a href="#" class="btn th-btn btn-standard">Download</a>
 
-<!--
-
-<!-- Ghost button example ->
+<!-- Ghost button example -->
 <a href="#" class="btn th-btn btn-ghost">Download</a>
 
-<!-- CTA button example ->
+<!-- CTA button example -->
 <a href="#" class="btn th-btn btn-cta">Download</a>
 
-<!-- Standard button example with icon ->
+<!-- Standard button example with icon -->
 <a href="#" class="btn th-btn btn-standard">
     Download
-    <i class="glyphicons glyphicons-cloud-upload"></i>
+    <span class="sow-icon-glyphicons" data-sow-icon="&#xe602;" ></span>
 </a>
 
-<!-- Image button example ->
+<!-- Image button example -->
 <a href="#" target="_blank" title="Buy on Google Play" class="th-btn btn-image"><img src="http://placehold.it/170x60" alt=""></a>
 
-<!-- Add to Cart button example ->
-do_shortcode [add_to_cart id="123" sku=""]
+<!-- Add to Cart button example -->
+<?php do_shortcode('[add_to_cart id="123" sku=""]')?>
 
--->
