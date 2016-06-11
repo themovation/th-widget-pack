a593af0a4480c5aa5a63f9fa4cce959006f8e8e5
adiraomj <adiraomj@outlook.com>
2016-06-11 23:55:46 +0530

    Removed SiteOrigin's row styling

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -8,3 +8,13 @@ function themovation_so_wb_admin() {
 }
 endif;
 add_action('admin_enqueue_scripts', 'themovation_so_wb_admin' );
+
+if ( ! function_exists ( 'themovation_so_wb_scripts' ) ) :
+// Enqueueing Frontend stylesheet and scripts.
+function themovation_so_wb_scripts() {
+
+	wp_enqueue_script( 'themo-main-js', plugin_dir_url(__FILE__) . '../js/themovation.js', array(), ​THEMOVATION_WB_VER, true );
+
+}
+endif;
+add_action( 'wp_enqueue_scripts', 'themovation_so_wb_scripts' );
---
+++ js/themovation.js
@@ -0,0 +1,7 @@
+jQuery( function ( $ ) {
+
+	$('.panel-row-style').removeClass('siteorigin-panels-stretch');
+	$('.panel-row-style').removeClass('panel-row-style-full-width');
+	$('.panel-row-style').removeAttr('data-stretch-type');
+
+} );
