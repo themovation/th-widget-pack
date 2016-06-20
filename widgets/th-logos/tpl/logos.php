a1f55cf33d0d58e5512454693c29267760c126c4
Teagan <teaganmckay@gmail.com>
2016-06-20 15:55:33 -0700

    Updated to add linked example

--- widgets/th-logos/tpl/logos.php
+++ widgets/th-logos/tpl/logos.php
@@ -1,7 +1,7 @@
 <div class="th-logos">
-    <img class="th-logo-img" src="http://placehold.it/150x80" width="150" height="80">
-    <img class="th-logo-img" src="http://placehold.it/150x80" width="150" height="80">
-    <img class="th-logo-img" src="http://placehold.it/150x80" width="150" height="80">
-    <img class="th-logo-img" src="http://placehold.it/150x80" width="150" height="80">
-    <img class="th-logo-img" src="http://placehold.it/150x80" width="150" height="80">
+    <img class="th-logo-img" src="http://placehold.it/150x80" width="150" height="80" alt="Logo">
+    <img class="th-logo-img" src="http://placehold.it/150x80" width="150" height="80" alt="Logo">
+    <img class="th-logo-img" src="http://placehold.it/150x80" width="150" height="80" alt="Logo">
+    <img class="th-logo-img" src="http://placehold.it/150x80" width="150" height="80" alt="Logo">
+    <a target="_blank" href="http://www.apple.com" data-toggle="lightbox" data-width="1280"><img class="th-logo-img" src="http://placehold.it/150x80" width="150" height="80" alt="Logo"></a>
 </div>
