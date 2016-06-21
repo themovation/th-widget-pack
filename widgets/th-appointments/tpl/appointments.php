c94a6eb3b858340894ac1742f6325e541f0dabcd
Teagan <teaganmckay@gmail.com>
2016-06-21 16:51:57 -0700

    Added Booked widget examples

---
+++ widgets/th-appointments/tpl/appointments.php
@@ -0,0 +1,22 @@
+<!-- Large example -->
+<div class="th-booked-cal-lg">
+    <div class="th-cal-tooltip"><h3>Book An Appointment Today</h3></div>
+    <?php echo do_shortcode('[booked-calendar]') ?>
+</div>
+
+<!-- Medium example -->
+<div class="th-booked-cal-med">
+    <div class="th-cal-tooltip"><h3>Book An Appointment Today</h3></div>
+    <?php echo do_shortcode('[booked-calendar]') ?>
+</div>
+
+<!-- Small example -->
+<div class="th-booked-cal-sm">
+    <div class="th-cal-tooltip"><h3>Book An Appointment Today</h3></div>
+    <?php echo do_shortcode('[booked-calendar]') ?>
+</div>
+
+<!-- No tooltip example -->
+<div class="th-booked-cal-sm">
+    <?php echo do_shortcode('[booked-calendar]') ?>
+</div>
