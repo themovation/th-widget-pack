abcfeb8b423550f7dfc18182fb259da2f3516814
Teagan <teaganmckay@gmail.com>
2016-06-22 17:51:36 -0700

    Added Formidable form examples

---
+++ widgets/th-formidable-forms/tpl/formidable-forms.php
@@ -0,0 +1,9 @@
+<!-- Default example -->
+<div class="th-formidable">
+    <?php echo do_shortcode('[formidable id=3]') ?>
+</div>
+
+<!-- Conversion form example -->
+<div class="th-formidable th-conversion">
+    <?php echo do_shortcode('[formidable id=3]') ?>
+</div>
