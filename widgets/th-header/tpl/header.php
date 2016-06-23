debc0431615f2e0416167e2346cfdab7d3b0177e
Teagan <teaganmckay@gmail.com>
2016-06-22 18:57:14 -0700

    Added Header examples and basic CSS

---
+++ widgets/th-header/tpl/header.php
@@ -0,0 +1,20 @@
+<!-- Left example -->
+<div class="th-header th-left">
+    <span class="sow-icon-glyphicons" data-sow-icon="&#xe602;" ></span>
+    <h2>Sample Title</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel turpis sed felis finibus scelerisque.</p>
+</div>
+
+<!-- Centered example -->
+<div class="th-header th-center">
+    <span class="sow-icon-glyphicons" data-sow-icon="&#xe602;" ></span>
+    <h2>Sample Title</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel turpis sed felis finibus scelerisque.</p>
+</div>
+
+<!-- Right example -->
+<div class="th-header th-right">
+    <span class="sow-icon-glyphicons" data-sow-icon="&#xe602;" ></span>
+    <h2>Sample Title</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel turpis sed felis finibus scelerisque.</p>
+</div>
