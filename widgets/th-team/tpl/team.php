f6aa2ef2ab675adbe66fffcd6294e4409d2c3e8e
Teagan <teaganmckay@gmail.com>
2016-06-24 14:51:53 -0700

    Added linked example

--- widgets/th-team/tpl/team.php
+++ widgets/th-team/tpl/team.php
@@ -1,7 +1,26 @@
+<!-- Standard example -->
 <div class="th-team-member">
     <div class="th-team-member-wrap">
         <img class="th-team-member-image" src="http://placehold.it/480x320">
         <h4>Chris Anderson</h4>
+        <h5>Chief Product Officer</h5>
+        <div class="th-team-member-bio">
+            <p>Chris leads Product Management, Design, and Marketing functions globally. He is responsible for the core products and features.</p>
+        </div>
+        <div class="th-team-member-social">
+            <a target="_blank" href=""><i class="soc-icon social social-facebook"></i></a>
+            <a target="_blank" href=""><i class="soc-icon social social-twitter"></i></a>
+        </div>
+    </div>
+</div>
+
+<!-- Example with link -->
+<div class="th-team-member">
+    <div class="th-team-member-wrap">
+        <a href="http://www.google.com">
+            <img class="th-team-member-image" src="http://placehold.it/480x320">
+            <h4>Chris Anderson</h4>
+        </a>
         <h5>Chief Product Officer</h5>
         <div class="th-team-member-bio">
             <p>Chris leads Product Management, Design, and Marketing functions globally. He is responsible for the core products and features.</p>
