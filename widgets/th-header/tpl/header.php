b92dad3fb11c7992a3f026100f0c49aa6e598cd6
adiraomj <adiraomj@outlook.com>
2016-06-28 09:35:43 +0530

    Updated header widget template

--- widgets/th-header/tpl/header.php
+++ widgets/th-header/tpl/header.php
@@ -1,20 +1,5 @@
-<!-- Left example -->
-<div class="th-header th-left">
-    <span class="sow-icon-glyphicons" data-sow-icon="&#xe602;" ></span>
-    <h2>Sample Title</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel turpis sed felis finibus scelerisque.</p>
-</div>
-
-<!-- Centered example -->
-<div class="th-header th-centered">
-    <span class="sow-icon-glyphicons" data-sow-icon="&#xe602;" ></span>
-    <h2>Sample Title</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel turpis sed felis finibus scelerisque.</p>
-</div>
-
-<!-- Right example -->
-<div class="th-header th-right">
-    <span class="sow-icon-glyphicons" data-sow-icon="&#xe602;" ></span>
-    <h2>Sample Title</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel turpis sed felis finibus scelerisque.</p>
+<div class="th-header th-<?php echo $instance['align'] ?>">
+	<?php themo_display_icon( $instance['icon'] ); ?>
+	<h2><?php echo esc_html( $instance['title'] ); ?></h2>
+	<?php echo wp_kses_post( $instance['content'] ); ?>
 </div>
