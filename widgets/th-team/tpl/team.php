4d4ab922e367ceb81c9844b95433874f1cc43819
Teagan <teaganmckay@gmail.com>
2016-07-12 18:12:29 -0700

    Added initial markup and CSS for Team widget

--- widgets/th-team/tpl/team.php
+++ widgets/th-team/tpl/team.php
@@ -1,5 +1,5 @@
 <!-- Standard example -->
-<div class="th-team-member">
+<div class="th-team-member th-dark-text">
     <div class="th-team-member-wrap">
         <img class="th-team-member-image" src="http://placehold.it/480x320">
         <h4>Chris Anderson</h4>
@@ -8,14 +8,15 @@
             <p>Chris leads Product Management, Design, and Marketing functions globally. He is responsible for the core products and features.</p>
         </div>
         <div class="th-team-member-social">
-            <a target="_blank" href=""><i class="soc-icon social social-facebook"></i></a>
-            <a target="_blank" href=""><i class="soc-icon social social-twitter"></i></a>
+            <a target="_blank" href="#"><span class="sow-icon-glyphsocial" data-sow-icon="&#xe031" ></span></a>
+            <a target="_blank" href="#"><span class="sow-icon-glyphsocial" data-sow-icon="&#xe031" ></span></a>
         </div>
     </div>
 </div>
 
+
 <!-- Example with link -->
-<div class="th-team-member">
+<div class="th-team-member th-dark-text">
     <div class="th-team-member-wrap">
         <a href="http://www.google.com">
             <img class="th-team-member-image" src="http://placehold.it/480x320">
@@ -26,8 +27,42 @@
             <p>Chris leads Product Management, Design, and Marketing functions globally. He is responsible for the core products and features.</p>
         </div>
         <div class="th-team-member-social">
-            <a target="_blank" href=""><i class="soc-icon social social-facebook"></i></a>
-            <a target="_blank" href=""><i class="soc-icon social social-twitter"></i></a>
+            <a target="_blank" href="#"><span class="sow-icon-glyphsocial" data-sow-icon="&#xe031" ></span></a>
+            <a target="_blank" href="#"><span class="sow-icon-glyphsocial" data-sow-icon="&#xe031" ></span></a>
+        </div>
+    </div>
+</div>
+
+
+<!-- Example with background and dark text -->
+<div class="th-team-member th-dark-text">
+    <div class="th-team-member-wrap" style="background-color:#e8e8e8">
+        <img class="th-team-member-image" src="http://placehold.it/480x320">
+        <h4>Chris Anderson</h4>
+        <h5>Chief Product Officer</h5>
+        <div class="th-team-member-bio">
+            <p>Chris leads Product Management, Design, and Marketing functions globally. He is responsible for the core products and features.</p>
+        </div>
+        <div class="th-team-member-social">
+            <a target="_blank" href="#"><span class="sow-icon-glyphsocial" data-sow-icon="&#xe031" ></span></a>
+            <a target="_blank" href="#"><span class="sow-icon-glyphsocial" data-sow-icon="&#xe031" ></span></a>
+        </div>
+    </div>
+</div>
+
+
+<!-- Example with background and light text -->
+<div class="th-team-member th-light-text">
+    <div class="th-team-member-wrap" style="background-color:#272727">
+        <img class="th-team-member-image" src="http://placehold.it/480x320">
+        <h4>Chris Anderson</h4>
+        <h5>Chief Product Officer</h5>
+        <div class="th-team-member-bio">
+            <p>Chris leads Product Management, Design, and Marketing functions globally. He is responsible for the core products and features.</p>
+        </div>
+        <div class="th-team-member-social">
+            <a target="_blank" href="#"><span class="sow-icon-glyphsocial" data-sow-icon="&#xe031" ></span></a>
+            <a target="_blank" href="#"><span class="sow-icon-glyphsocial" data-sow-icon="&#xe031" ></span></a>
         </div>
     </div>
 </div>
