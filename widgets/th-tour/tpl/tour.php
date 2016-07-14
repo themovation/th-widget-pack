cd68a08809c1fb2c4a5bfcf1c8e486c4ddd28242
Teagan <teaganmckay@gmail.com>
2016-07-14 16:03:11 -0700

    Expanded Tour examples

--- widgets/th-tour/tpl/tour.php
+++ widgets/th-tour/tpl/tour.php
@@ -4,7 +4,7 @@
         <div class="float-content col-sm-8">
             <div class="center-table-con">
                 <div class="center-table-cell">
-                    <h2 class="tour-content-title">Build Your Theme Using Meta Boxes</h2>
+                    <h2 class="tour-content-title">Tour Right Image Example</h2>
                     <div class="tour-content">
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ultrices nunc eu varius egestas. Etiam ultricies laoreet velit sed gravida. Phasellus malesuada, sem eget tempus vestibulum, turpis velit tempor ante, a porta odio risus in nisi. In hac habitasse platea dictumst.</p>
                     </div>
@@ -13,7 +13,53 @@
         </div>
         <div class="float-img col-sm-4">
             <div class="center-table-con">
-                <div class="center-table-cell ">
+                <div class="center-table-cell">
+                    <img class="img-responsive" src="http://placehold.it/480x320">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Left aligned image example -->
+<div class="th-tour img-left">
+    <div class="row">
+        <div class="float-content col-sm-8">
+            <div class="center-table-con">
+                <div class="center-table-cell">
+                    <h2 class="tour-content-title">Tour Left Image Example</h2>
+                    <div class="tour-content">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ultrices nunc eu varius egestas. Etiam ultricies laoreet velit sed gravida. Phasellus malesuada, sem eget tempus vestibulum, turpis velit tempor ante, a porta odio risus in nisi. In hac habitasse platea dictumst.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="float-img col-sm-4">
+            <div class="center-table-con">
+                <div class="center-table-cell">
+                    <img class="img-responsive" src="http://placehold.it/480x320">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Center aligned image example -->
+<div class="th-tour img-center">
+    <div class="row">
+        <div class="float-content col-sm-8">
+            <div class="center-table-con">
+                <div class="center-table-cell">
+                    <h2 class="tour-content-title">Tour Left Image Example</h2>
+                    <div class="tour-content">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ultrices nunc eu varius egestas. Etiam ultricies laoreet velit sed gravida. Phasellus malesuada, sem eget tempus vestibulum, turpis velit tempor ante, a porta odio risus in nisi. In hac habitasse platea dictumst.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="float-img col-sm-4">
+            <div class="center-table-con">
+                <div class="center-table-cell">
                     <img class="img-responsive" src="http://placehold.it/480x320">
                 </div>
             </div>
