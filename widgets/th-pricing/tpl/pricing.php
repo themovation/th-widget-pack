dca3b1313a51a3f7c4dd36a6bd80db65f37764ce
Teagan <teaganmckay@gmail.com>
2016-07-21 18:17:00 -0700

    Added pricing table examples

---
+++ widgets/th-pricing/tpl/pricing.php
@@ -0,0 +1,392 @@
+ <!-- One column example -->
+ <div class="th-pricing-table th-one-col">
+     <div class="row">
+         <div class="th-pricing-column col-sm-6 col-sm-offset-3">
+             <div class="th-pricing-cost">
+                 $50
+                 <span>/month</span>
+             </div>
+             <div class="th-pricing-title">15 Day Trial</div>
+             <div class="th-pricing-features">
+                 <ul>
+                     <li>Customer Support</li>
+                     <li>Custom Domain</li>
+                     <li>Lots of Storage</li>
+                     <li>High Speed Bandwidth</li>
+                     <li>Premium Design</li>
+                 </ul>
+                 <a href="#" class="btn btn-ghost th-btn">Get Started</a>
+             </div>
+         </div>
+     </div>
+ </div>
+
+ <!-- Two column example -->
+ <div class="th-pricing-table th-two-col">
+     <div class="row">
+         <div class="th-pricing-column col-sm-6">
+             <div class="th-pricing-cost">
+                 $50
+                 <span>/month</span>
+             </div>
+             <div class="th-pricing-title">15 Day Trial</div>
+             <div class="th-pricing-features">
+                 <ul>
+                     <li>Customer Support</li>
+                     <li>Custom Domain</li>
+                     <li>Lots of Storage</li>
+                     <li>High Speed Bandwidth</li>
+                     <li>Premium Design</li>
+                 </ul>
+                 <a href="#" class="btn btn-ghost th-btn">Get Started</a>
+             </div>
+         </div>
+         <div class="th-pricing-column col-sm-6">
+             <div class="th-pricing-cost">
+                 $50
+                 <span>/month</span>
+             </div>
+             <div class="th-pricing-title">15 Day Trial</div>
+             <div class="th-pricing-features">
+                 <ul>
+                     <li>Customer Support</li>
+                     <li>Custom Domain</li>
+                     <li>Lots of Storage</li>
+                     <li>High Speed Bandwidth</li>
+                     <li>Premium Design</li>
+                 </ul>
+                 <a href="#" class="btn btn-ghost th-btn">Get Started</a>
+             </div>
+         </div>
+     </div>
+ </div>
+
+ <!-- Three column example with highlight -->
+ <div class="th-pricing-table th-three-col">
+     <div class="row">
+         <div class="th-pricing-column col-md-4 col-sm-6">
+             <div class="th-pricing-cost">
+                 $50
+                 <span>/month</span>
+             </div>
+             <div class="th-pricing-title">15 Day Trial</div>
+             <div class="th-pricing-features">
+                 <ul>
+                     <li>Customer Support</li>
+                     <li>Custom Domain</li>
+                     <li>Lots of Storage</li>
+                     <li>High Speed Bandwidth</li>
+                     <li>Premium Design</li>
+                 </ul>
+                 <a href="#" class="btn btn-ghost th-btn">Get Started</a>
+             </div>
+         </div>
+         <div class="th-pricing-column th-highlight col-md-4 col-sm-6">
+             <div class="th-pricing-cost">
+                 $50
+                 <span>/month</span>
+             </div>
+             <div class="th-pricing-title">15 Day Trial</div>
+             <div class="th-pricing-features">
+                 <ul>
+                     <li>Customer Support</li>
+                     <li>Custom Domain</li>
+                     <li>Lots of Storage</li>
+                     <li>High Speed Bandwidth</li>
+                     <li>Premium Design</li>
+                 </ul>
+                 <a href="#" class="btn btn-ghost th-btn">Get Started</a>
+             </div>
+         </div>
+         <div class="th-pricing-column col-md-4 col-sm-6">
+             <div class="th-pricing-cost">
+                 $50
+                 <span>/month</span>
+             </div>
+             <div class="th-pricing-title">15 Day Trial</div>
+             <div class="th-pricing-features">
+                 <ul>
+                     <li>Customer Support</li>
+                     <li>Custom Domain</li>
+                     <li>Lots of Storage</li>
+                     <li>High Speed Bandwidth</li>
+                     <li>Premium Design</li>
+                 </ul>
+                 <a href="#" class="btn btn-ghost th-btn">Get Started</a>
+             </div>
+         </div>
+     </div>
+ </div>
+
+ <!-- Four column example -->
+ <div class="th-pricing-table th-four-col">
+     <div class="row">
+         <div class="th-pricing-column col-md-3 col-sm-6">
+             <div class="th-pricing-cost">
+                 $50
+                 <span>/month</span>
+             </div>
+             <div class="th-pricing-title">15 Day Trial</div>
+             <div class="th-pricing-features">
+                 <ul>
+                     <li>Customer Support</li>
+                     <li>Custom Domain</li>
+                     <li>Lots of Storage</li>
+                     <li>High Speed Bandwidth</li>
+                     <li>Premium Design</li>
+                 </ul>
+                 <a href="#" class="btn btn-ghost th-btn">Get Started</a>
+             </div>
+         </div>
+         <div class="th-pricing-column col-md-3 col-sm-6">
+             <div class="th-pricing-cost">
+                 $50
+                 <span>/month</span>
+             </div>
+             <div class="th-pricing-title">15 Day Trial</div>
+             <div class="th-pricing-features">
+                 <ul>
+                     <li>Customer Support</li>
+                     <li>Custom Domain</li>
+                     <li>Lots of Storage</li>
+                     <li>High Speed Bandwidth</li>
+                     <li>Premium Design</li>
+                 </ul>
+                 <a href="#" class="btn btn-ghost th-btn">Get Started</a>
+             </div>
+         </div>
+         <div class="th-pricing-column col-md-3 col-sm-6">
+             <div class="th-pricing-cost">
+                 $50
+                 <span>/month</span>
+             </div>
+             <div class="th-pricing-title">15 Day Trial</div>
+             <div class="th-pricing-features">
+                 <ul>
+                     <li>Customer Support</li>
+                     <li>Custom Domain</li>
+                     <li>Lots of Storage</li>
+                     <li>High Speed Bandwidth</li>
+                     <li>Premium Design</li>
+                 </ul>
+                 <a href="#" class="btn btn-ghost th-btn">Get Started</a>
+             </div>
+         </div>
+         <div class="th-pricing-column col-md-3 col-sm-6">
+             <div class="th-pricing-cost">
+                 $50
+                 <span>/month</span>
+             </div>
+             <div class="th-pricing-title">15 Day Trial</div>
+             <div class="th-pricing-features">
+                 <ul>
+                     <li>Customer Support</li>
+                     <li>Custom Domain</li>
+                     <li>Lots of Storage</li>
+                     <li>High Speed Bandwidth</li>
+                     <li>Premium Design</li>
+                 </ul>
+                 <a href="#" class="btn btn-ghost th-btn">Get Started</a>
+             </div>
+         </div>
+     </div>
+ </div>
+
+ <!-- Five column example -->
+ <div class="th-pricing-table th-five-col">
+     <div class="row">
+         <div class="th-pricing-column col-md-2 col-sm-6">
+             <div class="th-pricing-cost">
+                 $50
+                 <span>/month</span>
+             </div>
+             <div class="th-pricing-title">15 Day Trial</div>
+             <div class="th-pricing-features">
+                 <ul>
+                     <li>Customer Support</li>
+                     <li>Custom Domain</li>
+                     <li>Lots of Storage</li>
+                     <li>High Speed Bandwidth</li>
+                     <li>Premium Design</li>
+                 </ul>
+                 <a href="#" class="btn btn-ghost th-btn">Get Started</a>
+             </div>
+         </div>
+         <div class="th-pricing-column col-md-2 col-sm-6">
+             <div class="th-pricing-cost">
+                 $50
+                 <span>/month</span>
+             </div>
+             <div class="th-pricing-title">15 Day Trial</div>
+             <div class="th-pricing-features">
+                 <ul>
+                     <li>Customer Support</li>
+                     <li>Custom Domain</li>
+                     <li>Lots of Storage</li>
+                     <li>High Speed Bandwidth</li>
+                     <li>Premium Design</li>
+                 </ul>
+                 <a href="#" class="btn btn-ghost th-btn">Get Started</a>
+             </div>
+         </div>
+         <div class="th-pricing-column col-md-2 col-sm-6">
+             <div class="th-pricing-cost">
+                 $50
+                 <span>/month</span>
+             </div>
+             <div class="th-pricing-title">15 Day Trial</div>
+             <div class="th-pricing-features">
+                 <ul>
+                     <li>Customer Support</li>
+                     <li>Custom Domain</li>
+                     <li>Lots of Storage</li>
+                     <li>High Speed Bandwidth</li>
+                     <li>Premium Design</li>
+                 </ul>
+                 <a href="#" class="btn btn-ghost th-btn">Get Started</a>
+             </div>
+         </div>
+         <div class="th-pricing-column col-md-2 col-sm-6">
+             <div class="th-pricing-cost">
+                 $50
+                 <span>/month</span>
+             </div>
+             <div class="th-pricing-title">15 Day Trial</div>
+             <div class="th-pricing-features">
+                 <ul>
+                     <li>Customer Support</li>
+                     <li>Custom Domain</li>
+                     <li>Lots of Storage</li>
+                     <li>High Speed Bandwidth</li>
+                     <li>Premium Design</li>
+                 </ul>
+                 <a href="#" class="btn btn-ghost th-btn">Get Started</a>
+             </div>
+         </div>
+         <div class="th-pricing-column col-md-2 col-sm-6">
+             <div class="th-pricing-cost">
+                 $50
+                 <span>/month</span>
+             </div>
+             <div class="th-pricing-title">15 Day Trial</div>
+             <div class="th-pricing-features">
+                 <ul>
+                     <li>Customer Support</li>
+                     <li>Custom Domain</li>
+                     <li>Lots of Storage</li>
+                     <li>High Speed Bandwidth</li>
+                     <li>Premium Design</li>
+                 </ul>
+                 <a href="#" class="btn btn-ghost th-btn">Get Started</a>
+             </div>
+         </div>
+     </div>
+ </div>
+
+ <!-- Six column example -->
+ <div class="th-pricing-table th-six-col">
+     <div class="row">
+         <div class="th-pricing-column col-md-2 col-sm-6">
+             <div class="th-pricing-cost">
+                 $50
+                 <span>/month</span>
+             </div>
+             <div class="th-pricing-title">15 Day Trial</div>
+             <div class="th-pricing-features">
+                 <ul>
+                     <li>Customer Support</li>
+                     <li>Custom Domain</li>
+                     <li>Lots of Storage</li>
+                     <li>High Speed Bandwidth</li>
+                     <li>Premium Design</li>
+                 </ul>
+                 <a href="#" class="btn btn-ghost th-btn">Get Started</a>
+             </div>
+         </div>
+         <div class="th-pricing-column col-md-2 col-sm-6">
+             <div class="th-pricing-cost">
+                 $50
+                 <span>/month</span>
+             </div>
+             <div class="th-pricing-title">15 Day Trial</div>
+             <div class="th-pricing-features">
+                 <ul>
+                     <li>Customer Support</li>
+                     <li>Custom Domain</li>
+                     <li>Lots of Storage</li>
+                     <li>High Speed Bandwidth</li>
+                     <li>Premium Design</li>
+                 </ul>
+                 <a href="#" class="btn btn-ghost th-btn">Get Started</a>
+             </div>
+         </div>
+         <div class="th-pricing-column col-md-2 col-sm-6">
+             <div class="th-pricing-cost">
+                 $50
+                 <span>/month</span>
+             </div>
+             <div class="th-pricing-title">15 Day Trial</div>
+             <div class="th-pricing-features">
+                 <ul>
+                     <li>Customer Support</li>
+                     <li>Custom Domain</li>
+                     <li>Lots of Storage</li>
+                     <li>High Speed Bandwidth</li>
+                     <li>Premium Design</li>
+                 </ul>
+                 <a href="#" class="btn btn-ghost th-btn">Get Started</a>
+             </div>
+         </div>
+         <div class="th-pricing-column col-md-2 col-sm-6">
+             <div class="th-pricing-cost">
+                 $50
+                 <span>/month</span>
+             </div>
+             <div class="th-pricing-title">15 Day Trial</div>
+             <div class="th-pricing-features">
+                 <ul>
+                     <li>Customer Support</li>
+                     <li>Custom Domain</li>
+                     <li>Lots of Storage</li>
+                     <li>High Speed Bandwidth</li>
+                     <li>Premium Design</li>
+                 </ul>
+                 <a href="#" class="btn btn-ghost th-btn">Get Started</a>
+             </div>
+         </div>
+         <div class="th-pricing-column col-md-2 col-sm-6">
+             <div class="th-pricing-cost">
+                 $50
+                 <span>/month</span>
+             </div>
+             <div class="th-pricing-title">15 Day Trial</div>
+             <div class="th-pricing-features">
+                 <ul>
+                     <li>Customer Support</li>
+                     <li>Custom Domain</li>
+                     <li>Lots of Storage</li>
+                     <li>High Speed Bandwidth</li>
+                     <li>Premium Design</li>
+                 </ul>
+                 <a href="#" class="btn btn-ghost th-btn">Get Started</a>
+             </div>
+         </div>
+         <div class="th-pricing-column col-md-2 col-sm-6">
+             <div class="th-pricing-cost">
+                 $50
+                 <span>/month</span>
+             </div>
+             <div class="th-pricing-title">15 Day Trial</div>
+             <div class="th-pricing-features">
+                 <ul>
+                     <li>Customer Support</li>
+                     <li>Custom Domain</li>
+                     <li>Lots of Storage</li>
+                     <li>High Speed Bandwidth</li>
+                     <li>Premium Design</li>
+                 </ul>
+                 <a href="#" class="btn btn-ghost th-btn">Get Started</a>
+             </div>
+         </div>
+     </div>
+ </div>
