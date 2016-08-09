a5ba5781c6d3006d0d52ce633124fd163fe56cb3
Teagan <teaganmckay@gmail.com>
2016-08-09 12:26:36 -0700

    Added second button to example

--- widgets/th-accordions/tpl/accordion.php
+++ widgets/th-accordions/tpl/accordion.php
@@ -17,6 +17,7 @@
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget posuere nisl. Fusce tincidunt massa pulvinar est lobortis, at pellentesque ante accumsan. Aenean condimentum neque a libero sollicitudin, a pretium massa auctor. Donec eu tellus scelerisque, gravida purus nec, fermentum lectus. Pellentesque sagittis eu mi ac dignissim. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Integer at mauris at leo dictum semper ac at neque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In eu posuere justo, at malesuada sem. In nibh enim, aliquam eu eros quis, ullamcorper pretium felis. Donec ac sagittis odio, vel volutpat ante. Fusce eget lacus mattis, venenatis lectus vel, vestibulum libero. Donec vulputate tristique libero.</p>
                     <div class="accordion-btn">
                         <a href="#" class="btn btn-ghost th-btn">Apply Now!</a>
+                        <a href="#" class="btn btn-standard th-btn">More Info</a>
                     </div>
                 </div>
             </div>
