a67da447b85cda9b8b4ddba0b8925fad1e5f8c7f
adiraomj <adiraomj@outlook.com>
2016-08-19 23:46:03 +0530

    Implemented use of waypoints for widget animations

--- js/themovation.js
+++ js/themovation.js
@@ -16,4 +16,15 @@
 
 jQuery( function ( $ ) {
 	$(document).wrapGridCell();
+
+	$( '.panel-grid' ).find('.so-panel.widget .th-hide-animation').waypoint( {
+		offset: function() {
+			return Waypoint.viewportHeight() - 100
+		},
+		handler: function() {
+			$(this).each(function(){
+				$(this).removeClass( 'th-hide-animation' );
+			});
+		}
+	});
 } );
