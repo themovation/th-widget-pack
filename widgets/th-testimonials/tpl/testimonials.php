d0f0372c0341c6d1e19d2a299e744e8460dfd635
Teagan <teaganmckay@gmail.com>
2016-08-24 15:19:15 -0700

    Added star rating functionality

--- widgets/th-testimonials/tpl/testimonials.php
+++ widgets/th-testimonials/tpl/testimonials.php
@@ -1,9 +1,17 @@
 <div class="th-testimonials">
     <div class="th-testimonial-single">
         <figure class="th-quote">
+            <div class="th-star-rating th-star-25">
+                <span class="th-star-1 glyphicons"></span>
+                <span class="th-star-2 glyphicons"></span>
+                <span class="th-star-3 glyphicons"></span>
+                <span class="th-star-4 glyphicons"></span>
+                <span class="th-star-5 glyphicons"></span>
+            </div>
             <blockquote>"Love this App. Proin venenatis, odio eget consectetur tincidunt, lacus magna."</blockquote>
             <img width="60" height="60" src="http://placehold.it/60x60" class="th-circle" alt="" />
             <figcaption>Kim Smith<span>Satisfied Customer</span></figcaption>
         </figure>
+
     </div>
 </div>
