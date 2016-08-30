b9c01225f9a8d0790ca5aa1e79123abbfdb6cf74
Teagan <teaganmckay@gmail.com>
2016-08-30 11:51:13 -0700

    Expanded examples to show 1,2,3 columns

--- widgets/th-testimonials/tpl/testimonials.php
+++ widgets/th-testimonials/tpl/testimonials.php
@@ -1,5 +1,7 @@
-<div class="th-testimonials">
-    <div class="th-testimonial-single">
+<!-- One per row example -->
+
+<div class="th-testimonials row">
+    <div class="th-testimonial-single col-md-12">
         <figure class="th-quote">
             <div class="th-star-rating th-star-25">
                 <span class="th-star-1 glyphicons"></span>
@@ -8,10 +10,94 @@
                 <span class="th-star-4 glyphicons"></span>
                 <span class="th-star-5 glyphicons"></span>
             </div>
-            <blockquote>"Love this App. Proin venenatis, odio eget consectetur tincidunt, lacus magna."</blockquote>
+            <blockquote>"Lorem ipsum dolor sit amet. Proin venenatis, odio eget consectetur tincidunt, lacus magna."</blockquote>
             <img width="60" height="60" src="http://placehold.it/60x60" class="th-circle" alt="" />
             <figcaption>Kim Smith<span>Satisfied Customer</span></figcaption>
         </figure>
+    </div>
+</div>
 
+
+<!-- Two per row example -->
+
+<div class="th-testimonials row">
+    <div class="th-testimonial-single col-md-6">
+        <figure class="th-quote">
+            <div class="th-star-rating th-star-25">
+                <span class="th-star-1 glyphicons"></span>
+                <span class="th-star-2 glyphicons"></span>
+                <span class="th-star-3 glyphicons"></span>
+                <span class="th-star-4 glyphicons"></span>
+                <span class="th-star-5 glyphicons"></span>
+            </div>
+            <blockquote>"Lorem ipsum dolor sit amet. Proin venenatis, odio eget consectetur tincidunt, lacus magna."</blockquote>
+            <img width="60" height="60" src="http://placehold.it/60x60" class="th-circle" alt="" />
+            <figcaption>Kim Smith<span>Satisfied Customer</span></figcaption>
+        </figure>
+    </div>
+
+    <div class="th-testimonial-single col-md-6">
+        <figure class="th-quote">
+            <div class="th-star-rating th-star-35">
+                <span class="th-star-1 glyphicons"></span>
+                <span class="th-star-2 glyphicons"></span>
+                <span class="th-star-3 glyphicons"></span>
+                <span class="th-star-4 glyphicons"></span>
+                <span class="th-star-5 glyphicons"></span>
+            </div>
+            <blockquote>"Lorem ipsum dolor sit amet. Proin venenatis, odio eget consectetur tincidunt, lacus magna."</blockquote>
+            <img width="60" height="60" src="http://placehold.it/60x60" class="th-circle" alt="" />
+            <figcaption>Jenn Blake<span>Satisfied Customer</span></figcaption>
+        </figure>
+    </div>
+</div>
+
+
+<!-- Three per row example -->
+
+<div class="th-testimonials row">
+    <div class="th-testimonial-single col-md-4">
+        <figure class="th-quote">
+            <div class="th-star-rating th-star-25">
+                <span class="th-star-1 glyphicons"></span>
+                <span class="th-star-2 glyphicons"></span>
+                <span class="th-star-3 glyphicons"></span>
+                <span class="th-star-4 glyphicons"></span>
+                <span class="th-star-5 glyphicons"></span>
+            </div>
+            <blockquote>"Lorem ipsum dolor sit amet. Proin venenatis, odio eget consectetur tincidunt, lacus magna."</blockquote>
+            <img width="60" height="60" src="http://placehold.it/60x60" class="th-circle" alt="" />
+            <figcaption>Kim Smith<span>Satisfied Customer</span></figcaption>
+        </figure>
+    </div>
+
+    <div class="th-testimonial-single col-md-4">
+        <figure class="th-quote">
+            <div class="th-star-rating th-star-35">
+                <span class="th-star-1 glyphicons"></span>
+                <span class="th-star-2 glyphicons"></span>
+                <span class="th-star-3 glyphicons"></span>
+                <span class="th-star-4 glyphicons"></span>
+                <span class="th-star-5 glyphicons"></span>
+            </div>
+            <blockquote>"Lorem ipsum dolor sit amet. Proin venenatis, odio eget consectetur tincidunt, lacus magna."</blockquote>
+            <img width="60" height="60" src="http://placehold.it/60x60" class="th-circle" alt="" />
+            <figcaption>Jenn Blake<span>Satisfied Customer</span></figcaption>
+        </figure>
+    </div>
+
+    <div class="th-testimonial-single col-md-4">
+        <figure class="th-quote">
+            <div class="th-star-rating th-star-45">
+                <span class="th-star-1 glyphicons"></span>
+                <span class="th-star-2 glyphicons"></span>
+                <span class="th-star-3 glyphicons"></span>
+                <span class="th-star-4 glyphicons"></span>
+                <span class="th-star-5 glyphicons"></span>
+            </div>
+            <blockquote>"Lorem ipsum dolor sit amet. Proin venenatis, odio eget consectetur tincidunt, lacus magna."</blockquote>
+            <img width="60" height="60" src="http://placehold.it/60x60" class="th-circle" alt="" />
+            <figcaption>Frank Speller<span>Satisfied Customer</span></figcaption>
+        </figure>
     </div>
 </div>
