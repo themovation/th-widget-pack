7c0685cfd4e71f924db02752b7cbda5cb9f0ec4e
Teagan <teaganmckay@gmail.com>
2016-08-31 12:34:34 -0700

    *temp* Testing code for Thumb slider

---
+++ widgets/th-package/tpl/package.php
@@ -0,0 +1,73 @@
+<!-- Portrait example -->
+
+<div id="sample-ID">
+    <div class="th-thumb-flex-slider flexslider th-flex-portrait">
+        <ul class="slides gallery">
+            <li class="th-thumb-flex-slider-img-1">
+                <a data-toggle="lightbox" data-gallery="multiimages" data-title="Placeholder"  class="th-img-thumbnail" href="https://placehold.it/510x900" data-parent="slides">
+                    <img width="255" height="450" src="https://placehold.it/255x450" class="img-responsive wp-post-image" alt="Placeholder" />
+                </a>
+                <p class="th-thumb-title">
+                    <a href="#">App Thumb Slide 1</a>
+                    <span>Text here!</span>
+                </p>
+            </li>
+            <li class="th-thumb-flex-slider-img-2">
+                <a data-toggle="lightbox" data-gallery="multiimages" data-title="Placeholder"  class="th-img-thumbnail" href="https://placehold.it/510x900" data-parent="slides">
+                    <img width="255" height="450" src="https://placehold.it/255x450" class="img-responsive wp-post-image" alt="Placeholder" />
+                </a>
+                <p class="th-thumb-title">
+                    <a href="#">App Thumb Slide 2</a>
+                    <span>Text here!</span>
+                </p>
+            </li>
+            <li class="th-thumb-flex-slider-img-3">
+                <a data-toggle="lightbox" data-gallery="multiimages" data-title="Placeholder"  class="th-img-thumbnail" href="https://placehold.it/510x900" data-parent="slides">
+                    <img width="255" height="450" src="https://placehold.it/255x450" class="img-responsive wp-post-image" alt="Placeholder" />
+                </a>
+                <p class="th-thumb-title">
+                    <a href="#">App Thumb Slide 3</a>
+                    <span>Text here!</span>
+                </p>
+            </li>
+            <li class="th-thumb-flex-slider-img-4">
+                <a data-toggle="lightbox" data-gallery="multiimages" data-title="Placeholder"  class="th-img-thumbnail" href="https://placehold.it/510x900" data-parent="slides">
+                    <img width="255" height="450" src="https://placehold.it/255x450" class="img-responsive wp-post-image" alt="Placeholder" />
+                </a>
+                <p class="th-thumb-title">
+                    <a href="#">App Thumb Slide 4</a>
+                    <span>Text here!</span>
+                </p>
+            </li>
+            <li class="th-thumb-flex-slider-img-5">
+                <a data-toggle="lightbox" data-gallery="multiimages" data-title="Placeholder"  class="th-img-thumbnail" href="https://placehold.it/510x900" data-parent="slides">
+                    <img width="255" height="450" src="https://placehold.it/255x450" class="img-responsive wp-post-image" alt="Placeholder" />
+                </a>
+                <p class="th-thumb-title">
+                    <a href="#">App Thumb Slide 5</a>
+                    <span>Text here!</span>
+                </p>
+            </li>
+        </ul>
+    </div>
+</div>
+
+
+<!-- Landscape example --> <!--
+
+<div class="th-thumb-flex-slider flexslider th-flex-landscape">
+</div>
+
+<!-- Square example --> <!--
+
+<div class="th-thumb-flex-slider flexslider th-flex-square">
+</div>
+
+-->
+
+<script>
+    jQuery(window).load(function() {
+        jQuery('.th-thumb-flex-slider').show();
+        themo_start_thumb_slider('#sample-ID');
+    });
+</script>
