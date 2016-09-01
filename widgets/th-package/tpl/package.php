5370ba717b6e98561c8761b525dc145bc556d843
Ryan Labelle <ryan@honeycombweb.com>
2016-09-01 10:37:37 -0700

    Moved container ID into flexslider div, then removed container DIV akak #sample-id

--- widgets/th-package/tpl/package.php
+++ widgets/th-package/tpl/package.php
@@ -1,55 +1,54 @@
 <!-- Portrait example -->
 
-<div id="sample-ID">
-    <div class="th-thumb-flex-slider flexslider th-flex-portrait">
-        <ul class="slides gallery">
-            <li class="th-thumb-flex-slider-img-1">
-                <a data-toggle="lightbox" data-gallery="multiimages" data-title="Placeholder"  class="th-img-thumbnail" href="https://placehold.it/510x900" data-parent="slides">
-                    <img width="255" height="450" src="https://placehold.it/255x450" class="img-responsive wp-post-image" alt="Placeholder" />
-                </a>
-                <p class="th-thumb-title">
-                    <a href="#">App Thumb Slide 1</a>
-                    <span>Text here!</span>
-                </p>
-            </li>
-            <li class="th-thumb-flex-slider-img-2">
-                <a data-toggle="lightbox" data-gallery="multiimages" data-title="Placeholder"  class="th-img-thumbnail" href="https://placehold.it/510x900" data-parent="slides">
-                    <img width="255" height="450" src="https://placehold.it/255x450" class="img-responsive wp-post-image" alt="Placeholder" />
-                </a>
-                <p class="th-thumb-title">
-                    <a href="#">App Thumb Slide 2</a>
-                    <span>Text here!</span>
-                </p>
-            </li>
-            <li class="th-thumb-flex-slider-img-3">
-                <a data-toggle="lightbox" data-gallery="multiimages" data-title="Placeholder"  class="th-img-thumbnail" href="https://placehold.it/510x900" data-parent="slides">
-                    <img width="255" height="450" src="https://placehold.it/255x450" class="img-responsive wp-post-image" alt="Placeholder" />
-                </a>
-                <p class="th-thumb-title">
-                    <a href="#">App Thumb Slide 3</a>
-                    <span>Text here!</span>
-                </p>
-            </li>
-            <li class="th-thumb-flex-slider-img-4">
-                <a data-toggle="lightbox" data-gallery="multiimages" data-title="Placeholder"  class="th-img-thumbnail" href="https://placehold.it/510x900" data-parent="slides">
-                    <img width="255" height="450" src="https://placehold.it/255x450" class="img-responsive wp-post-image" alt="Placeholder" />
-                </a>
-                <p class="th-thumb-title">
-                    <a href="#">App Thumb Slide 4</a>
-                    <span>Text here!</span>
-                </p>
-            </li>
-            <li class="th-thumb-flex-slider-img-5">
-                <a data-toggle="lightbox" data-gallery="multiimages" data-title="Placeholder"  class="th-img-thumbnail" href="https://placehold.it/510x900" data-parent="slides">
-                    <img width="255" height="450" src="https://placehold.it/255x450" class="img-responsive wp-post-image" alt="Placeholder" />
-                </a>
-                <p class="th-thumb-title">
-                    <a href="#">App Thumb Slide 5</a>
-                    <span>Text here!</span>
-                </p>
-            </li>
-        </ul>
-    </div>
+
+<div id="sample-ID" class="th-thumb-flex-slider flexslider th-flex-portrait">
+    <ul class="slides gallery">
+        <li class="th-thumb-flex-slider-img-1">
+            <a data-toggle="lightbox" data-gallery="multiimages" data-title="Placeholder"  class="th-img-thumbnail" href="https://placehold.it/510x900" data-parent="slides">
+                <img width="255" height="450" src="https://placehold.it/255x450" class="img-responsive wp-post-image" alt="Placeholder" />
+            </a>
+            <p class="th-thumb-title">
+                <a href="#">App Thumb Slide 1</a>
+                <span>Text here!</span>
+            </p>
+        </li>
+        <li class="th-thumb-flex-slider-img-2">
+            <a data-toggle="lightbox" data-gallery="multiimages" data-title="Placeholder"  class="th-img-thumbnail" href="https://placehold.it/510x900" data-parent="slides">
+                <img width="255" height="450" src="https://placehold.it/255x450" class="img-responsive wp-post-image" alt="Placeholder" />
+            </a>
+            <p class="th-thumb-title">
+                <a href="#">App Thumb Slide 2</a>
+                <span>Text here!</span>
+            </p>
+        </li>
+        <li class="th-thumb-flex-slider-img-3">
+            <a data-toggle="lightbox" data-gallery="multiimages" data-title="Placeholder"  class="th-img-thumbnail" href="https://placehold.it/510x900" data-parent="slides">
+                <img width="255" height="450" src="https://placehold.it/255x450" class="img-responsive wp-post-image" alt="Placeholder" />
+            </a>
+            <p class="th-thumb-title">
+                <a href="#">App Thumb Slide 3</a>
+                <span>Text here!</span>
+            </p>
+        </li>
+        <li class="th-thumb-flex-slider-img-4">
+            <a data-toggle="lightbox" data-gallery="multiimages" data-title="Placeholder"  class="th-img-thumbnail" href="https://placehold.it/510x900" data-parent="slides">
+                <img width="255" height="450" src="https://placehold.it/255x450" class="img-responsive wp-post-image" alt="Placeholder" />
+            </a>
+            <p class="th-thumb-title">
+                <a href="#">App Thumb Slide 4</a>
+                <span>Text here!</span>
+            </p>
+        </li>
+        <li class="th-thumb-flex-slider-img-5">
+            <a data-toggle="lightbox" data-gallery="multiimages" data-title="Placeholder"  class="th-img-thumbnail" href="https://placehold.it/510x900" data-parent="slides">
+                <img width="255" height="450" src="https://placehold.it/255x450" class="img-responsive wp-post-image" alt="Placeholder" />
+            </a>
+            <p class="th-thumb-title">
+                <a href="#">App Thumb Slide 5</a>
+                <span>Text here!</span>
+            </p>
+        </li>
+    </ul>
 </div>
 
 
