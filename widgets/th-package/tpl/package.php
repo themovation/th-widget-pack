a78240b2d4ceb57f7217ccce56d2cbfc52b18254
Teagan <teaganmckay@gmail.com>
2016-09-14 13:45:36 -0700

    Completed examples for options in Package widget

--- widgets/th-package/tpl/package.php
+++ widgets/th-package/tpl/package.php
@@ -1,14 +1,32 @@
+<!-- Standard example -->
 
 <article class="th-package">
     <a class="th-pkg-click" href="http://www.google.com"></a>
-    <div class="th-pkg-info">
+    <div class="th-pkg-info th-pkg-info-dark">
         <h4>30%</h4>
         <span>discount</span>
     </div>
     <div class="th-pkg-img">
         <img src="http://placehold.it/560x320">
     </div>
-    <div class="th-pkg-content">
+    <div class="th-pkg-content th-pkg-con-dark">
+        <h3>Nine and Dine</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce volutpat rutrum ligula, eu volutpat massa auctor eu. Nullam malesuada dui a lacus finibus pretium.</p>
+    </div>
+</article>
+
+<!-- Example with backgrounds specified -->
+
+<article class="th-package">
+    <a class="th-pkg-click" href="http://www.google.com"></a>
+    <div class="th-pkg-info th-pkg-info-light" style="background-color:rgba(0,0,0,0.8)">
+        <h4>30%</h4>
+        <span>discount</span>
+    </div>
+    <div class="th-pkg-img">
+        <img src="http://placehold.it/560x320">
+    </div>
+    <div class="th-pkg-content th-pkg-con-light" style="background-color:#424348">
         <h3>Nine and Dine</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce volutpat rutrum ligula, eu volutpat massa auctor eu. Nullam malesuada dui a lacus finibus pretium.</p>
     </div>
