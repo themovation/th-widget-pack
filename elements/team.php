de1b8c70cd87c7e9e4985e8a7e5de900f3a06096
adiraomj <adiraomj@outlook.com>
2016-12-06 19:41:44 +0530

    Rearranged team widget fields

--- elements/team.php
+++ elements/team.php
@@ -30,20 +30,20 @@ class Themo_Widget_Team extends Widget_Base {
 		);
 
 		$this->add_control(
-			'title',
+			'image',
 			[
-				'label' => __( 'Title', 'elementor' ),
-				'type' => Controls_Manager::TEXT,
-				'placeholder' => __( 'Enter title here', 'elementor' ),
-				'label_block' => true,
+				'label' => __( 'Image', 'elementor' ),
+				'type' => Controls_Manager::MEDIA,
+				'default' => '',
 			]
 		);
 
 		$this->add_control(
-			'content',
+			'name',
 			[
-				'label' => __( 'Content', 'elementor' ),
-				'type' => Controls_Manager::WYSIWYG,
+				'label' => __( 'Name', 'elementor' ),
+				'type' => Controls_Manager::TEXT,
+				'placeholder' => __( 'Enter name here', 'elementor' ),
 				'label_block' => true,
 			]
 		);
@@ -54,6 +54,15 @@ class Themo_Widget_Team extends Widget_Base {
 				'label' => __( 'Job Title', 'elementor' ),
 				'type' => Controls_Manager::TEXT,
 				'placeholder' => __( 'CEO', 'elementor' ),
+				'label_block' => true,
+			]
+		);
+
+		$this->add_control(
+			'content',
+			[
+				'label' => __( 'Content', 'elementor' ),
+				'type' => Controls_Manager::WYSIWYG,
 				'label_block' => true,
 			]
 		);
@@ -108,24 +117,6 @@ class Themo_Widget_Team extends Widget_Base {
 		$this->end_controls_section();
 
 		$this->start_controls_section(
-			'section_image',
-			[
-				'label' => __( 'Image', 'elementor' ),
-			]
-		);
-
-		$this->add_control(
-			'image',
-			[
-				'label' => __( 'Image', 'elementor' ),
-				'type' => Controls_Manager::MEDIA,
-				'default' => '',
-			]
-		);
-
-		$this->end_controls_section();
-
-		$this->start_controls_section(
 			'section_social',
 			[
 				'label' => __( 'Social Icons', 'elementor' ),
@@ -147,13 +138,6 @@ class Themo_Widget_Team extends Widget_Base {
 						'name' => 'icon',
 						'label' => __( 'Icon', 'elementor' ),
 						'type' => Controls_Manager::ICON,
-						'default' => '',
-						'label_block' => true,
-					],
-					[
-						'name' => 'graphic_image',
-						'label' => __( 'Graphic Image', 'elementor' ),
-						'type' => Controls_Manager::MEDIA,
 						'default' => '',
 						'label_block' => true,
 					],
@@ -187,7 +171,7 @@ class Themo_Widget_Team extends Widget_Base {
 						'label_block' => true,
 					]
 				],
-				'title_field' => '<i class="{{ icon }}"></i> {{{ url }}}',
+				'title_field' => '<i class="{{ icon }}"></i> {{{ url.url }}}',
 			]
 		);
 
@@ -212,26 +196,11 @@ class Themo_Widget_Team extends Widget_Base {
 			]
 		);
 
-		$this->add_control(
-			'background_contrast',
-			[
-				'label' => __( 'Contrast', 'elementor' ),
-				'type' => Controls_Manager::SELECT,
-				'default' => 'dark',
-				'options' => [
-					'dark' => __( 'Dark Text', 'elementor' ),
-					'light' => __( 'Light Text', 'elementor' ),
-				],
-			]
-		);
-
 		$this->end_controls_section();
 	}
 
 	protected function render() {
 		$settings = $this->get_settings();
-
-		$this->add_render_attribute('th-team-member','class', 'th-' . $settings['background_contrast'] . '-text');
 
 		if ( ! empty( $settings['url']['url'] ) ) {
 			$this->add_render_attribute( 'link', 'href', $settings['url']['url'] );
@@ -249,10 +218,10 @@ class Themo_Widget_Team extends Widget_Base {
 			}
 		}
 
-		if ( empty( $settings['title'] ) ) {
+		if ( empty( $settings['name'] ) ) {
 			return;
 		} else {
-			$title = '<h4>' . esc_html( $settings['title'] ) . '</h4>';
+			$name = '<h4>' . esc_html( $settings['name'] ) . '</h4>';
 		}
 
 		if ( $settings['image']['id'] ) $image = wp_get_attachment_image( $settings['image']['id'], 'th_img_sm_portrait', false, array( 'class' => 'th-team-member-image' ) );
@@ -262,10 +231,10 @@ class Themo_Widget_Team extends Widget_Base {
 			<div class="th-team-member-wrap">
 				<?php if ( ! empty( $settings['url']['url'] ) ) : ?>
 					<a <?php echo $this->get_render_attribute_string( 'link' ); ?>>
-						<?php echo $image . '' . $title; ?>
+						<?php echo $image . '' . $name; ?>
 					</a>
 				<?php else : ?>
-					<?php echo $image . '' . $title; ?>
+					<?php echo $image . '' . $name; ?>
 				<?php endif; ?>
 				<?php if ( ! empty( $settings['job'] ) ) : ?>
 					<h5><?php echo esc_html( $settings['job']) ?></h5>
@@ -284,11 +253,9 @@ class Themo_Widget_Team extends Widget_Base {
 
 							echo '<a href="' . $social['url']['url'] . '"' . $target . ' ' . $lightbox . ' ' . $lightbox_width . '>';
 						}
-							if ( $social['graphic_image']['id'] ) :
-								wp_get_attachment_image( $social['graphic_image']['id'], 'full', false, array( 'class' => 'th-icon th-icon-graphic' ) );
-							elseif ( $social['icon'] ) : ?>
-								<i class="<?php echo esc_attr( $social['icon'] ); ?>"></i>
-							<?php endif;
+						if ( $social['icon'] ) : ?>
+							<i class="<?php echo esc_attr( $social['icon'] ); ?>"></i>
+						<?php endif;
 						if ( ! empty( $social['url']['url'] ) ) {
 							echo '</a>';
 						}
@@ -302,7 +269,7 @@ class Themo_Widget_Team extends Widget_Base {
 
 	protected function _content_template() {
 		?>
-		<div class="th-team-member 'th-' + settings'background_contrast' + '-text'">
+		<div class="th-team-member">
 			<div class="th-team-member-wrap">
 				<# if ( settings.url && settings.url.url ) { #>
 					<a href="{{ settings.url.url }}">
@@ -310,8 +277,8 @@ class Themo_Widget_Team extends Widget_Base {
 					<# if ( settings.image && '' !== settings.image.url ) { #>
 						<img src="{{{ settings.image.url }}}" class="th-team-member-image" />
 					<# } #>
-					<# if ( '' !== settings.title ) { #>
-						<h4>{{{ settings.title }}}</h4>
+					<# if ( '' !== settings.name ) { #>
+						<h4>{{{ settings.name }}}</h4>
 					<# } #>
 				<# if ( settings.url && settings.url.url ) { #>
 					</a>
@@ -331,9 +298,6 @@ class Themo_Widget_Team extends Widget_Base {
 							<# if ( item.url && item.url.url ) { #>
 								<a href="{{ item.url.url }}">
 							<# } #>
-								<# if ( item.image && item.image.url ) { #>
-									<img src="{{{ item.image.url }}}" class="th-icon th-icon-graphic" />
-								<# } #>
 								<# if ( item.icon ) { #>
 									<i class="{{ item.icon }}"></i>
 								<# } #>
