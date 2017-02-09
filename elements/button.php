25a9bce8c951b73217dadc4c6833c125102d14e6
adiraomj <adiraomj@outlook.com>
2017-02-09 23:50:18 +0530

    Added template for button widget

--- elements/button.php
+++ elements/button.php
@@ -100,7 +100,7 @@ class Themo_Widget_Button extends Widget_Base {
 						'icon' => 'fa fa-align-justify',
 					],
 				],
-				'prefix_class' => 'elementor%s-align-',
+				'prefix_class' => 'btn-1%s-align-',
 				'default' => '',
 			]
 		);
@@ -185,7 +185,7 @@ class Themo_Widget_Button extends Widget_Base {
 						'icon' => 'fa fa-align-justify',
 					],
 				],
-				'prefix_class' => 'elementor%s-align-',
+				'prefix_class' => 'btn-2%s-align-',
 				'default' => '',
 			]
 		);
@@ -195,14 +195,77 @@ class Themo_Widget_Button extends Widget_Base {
 
 	protected function render() {
 		$settings = $this->get_settings();
+
+		$this->add_render_attribute( 'btn-1-link', 'class', 'btn-1' );
+		$this->add_render_attribute( 'btn-1-link', 'class', 'btn' );
+		$this->add_render_attribute( 'btn-1-link', 'class', 'th-btn' );
+		$this->add_render_attribute( 'btn-1-link', 'class', 'btn-' . $settings['button_1_style'] );
+
+		if ( ! empty( $settings['button_1_link']['url'] ) ) {
+			$this->add_render_attribute( 'btn-1-link', 'href', $settings['button_1_link']['url'] );
+
+			if ( ! empty( $settings['button_1_link']['is_external'] ) ) {
+				$this->add_render_attribute( 'btn-1-link', 'target', '_blank' );
+			}
+		}
+
+		$this->add_render_attribute( 'btn-2-link', 'class', 'btn-2' );
+		$this->add_render_attribute( 'btn-2-link', 'class', 'btn' );
+		$this->add_render_attribute( 'btn-2-link', 'class', 'th-btn' );
+		$this->add_render_attribute( 'btn-2-link', 'class', 'btn-' . $settings['button_2_style'] );
+
+		if ( ! empty( $settings['button_2_link']['url'] ) ) {
+			$this->add_render_attribute( 'btn-2-link', 'href', $settings['button_2_link']['url'] );
+
+			if ( ! empty( $settings['button_2_link']['is_external'] ) ) {
+				$this->add_render_attribute( 'btn-2-link', 'target', '_blank' );
+			}
+		}
 		?>
+
+		<?php if ( ! empty( $settings['button_1_link']['url'] ) ) : ?>
+			<a <?php echo $this->get_render_attribute_string( 'btn-1-link' ); ?>>
+				<?php if ( ! empty( $settings['button_1_text'] ) ) : ?>
+					<?php echo esc_html( $settings['button_1_text'] ); ?>
+				<?php endif; ?>
+				<?php if ( ! empty( $settings['button_1_icon'] ) ) : ?>
+					<i class="<?php echo esc_attr( $settings['button_1_icon'] ); ?>"></i>
+				<?php endif; ?>
+			</a>
+		<?php endif; ?>
+
+		<?php if ( ! empty( $settings['button_2_link']['url'] ) ) : ?>
+			<a <?php echo $this->get_render_attribute_string( 'btn-2-link' ); ?>>
+				<?php if ( ! empty( $settings['button_2_text'] ) ) : ?>
+					<?php echo esc_html( $settings['button_2_text'] ); ?>
+				<?php endif; ?>
+				<?php if ( ! empty( $settings['button_2_icon'] ) ) : ?>
+					<i class="<?php echo esc_attr( $settings['button_2_icon'] ); ?>"></i>
+				<?php endif; ?>
+			</a>
+		<?php endif; ?>
 
 		<?php
 	}
 
 	protected function _content_template() {
 		?>
-
+		<# if ( settings.button_1_link.url ) { #>
+			<a class="btn btn-1 th-btn btn-{{ settings.button_1_style }}" href="{{ settings.button_1_link.url }}">
+				{{{ settings.button_1_text }}}
+				<# if ( settings.button_1_icon ) { #>
+					<i class="{{ settings.button_1_icon }}"></i>
+				<# } #>
+			</a>
+		<# } #>
+		<# if ( settings.button_2_link.url ) { #>
+			<a class="btn btn-2 th-btn btn-{{ settings.button_2_style }}" href="{{ settings.button_2_link.url }}">
+				{{{ settings.button_1_text }}}
+				<# if ( settings.button_2_icon ) { #>
+					<i class="{{ settings.button_2_icon }}"></i>
+				<# } #>
+			</a>
+		<# } #>
 		<?php
 	}
 }
