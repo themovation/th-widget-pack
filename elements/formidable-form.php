8e91d54b177646b4eabb0ba8a8cc2ba1e4223d4e
ryanlabelle <ryan@honeycombweb.com>
2017-03-02 14:30:01 -0800

    Added th-fo-form class, new options for stacked, inline and default, border colour, and alignment. https://github.com/ryanlabelle/themovation-so-widgets-bundle/issues/95

--- elements/formidable-form.php
+++ elements/formidable-form.php
@@ -33,7 +33,7 @@ class Themo_Widget_Formidable extends Widget_Base {
 			]
 		);
 
-		$this->add_control(
+		/*$this->add_control(
 			'shortcode',
 			[
 				'label' => __( 'Insert your shortcode here', 'elementor' ),
@@ -41,9 +41,19 @@ class Themo_Widget_Formidable extends Widget_Base {
 				'default' => '[formidable id=3]',
 				'placeholder' => '[formidable id="3"]',
 			]
-		);
+		);*/
 
         $this->add_control(
+            'shortcode',
+            [
+                'label' => __( 'Shortcode', 'elementor' ),
+                'type' => Controls_Manager::TEXT,
+                'placeholder' => __( '[formidable id=3]', 'elementor' ),
+                'default' => __( '[formidable id=3]', 'elementor' )
+            ]
+        );
+
+        /*$this->add_control(
             'inline_form',
             [
                 'label' => __( 'Show form inline', 'elementor' ),
@@ -52,6 +62,61 @@ class Themo_Widget_Formidable extends Widget_Base {
                 'label_on' => __( 'Yes', 'elementor' ),
                 'label_off' => __( 'No', 'elementor' ),
                 'return_value' => 'th-conversion',
+            ]
+        );*/
+        $this->add_control(
+            'inline_form',
+            [
+                'label' => __( 'Formidable Form Style', 'elementor' ),
+                'type' => Controls_Manager::SELECT,
+                'default' => 'none',
+                'options' => [
+                    'none' => __( 'Default', 'elementor' ),
+                    'inline' => __( 'Inline', 'elementor' ),
+                    'stacked' => __( 'Stacked', 'elementor' ),
+
+                ],
+            ]
+        );
+        $this->add_control(
+            'slide_shortcode_border',
+            [
+                'label' => __( 'Form Background', 'elementor' ),
+                'type' => Controls_Manager::SELECT,
+                'default' => 'none',
+                'options' => [
+                    'none' => __( 'None', 'elementor' ),
+                    'th-form-bg th-light-bg' => __( 'Light', 'elementor' ),
+                    'th-form-bg th-dark-bg' => __( 'Dark', 'elementor' ),
+
+                ],
+                'condition' => [
+                    'inline_form' => 'stacked',
+                ],
+            ]
+        );
+
+        $this->add_control(
+            'slide_text_align',
+            [
+                'label' => __( 'Align', 'elementor' ),
+                'type' => Controls_Manager::CHOOSE,
+                'label_block' => false,
+                'options' => [
+                    'left' => [
+                        'title' => __( 'Left', 'elementor' ),
+                        'icon' => 'fa fa-align-left',
+                    ],
+                    'center' => [
+                        'title' => __( 'Center', 'elementor' ),
+                        'icon' => 'fa fa-align-center',
+                    ],
+                    'right' => [
+                        'title' => __( 'Right', 'elementor' ),
+                        'icon' => 'fa fa-align-right',
+                    ],
+                ],
+                'default' => 'center',
             ]
         );
 
@@ -63,9 +128,46 @@ class Themo_Widget_Formidable extends Widget_Base {
         $settings = $this->get_settings();
 
         if(isset($settings['shortcode']) && ! empty($settings['shortcode'])){
-            $th_shortcode = $settings['shortcode'];
+            $th_shortcode = sanitize_text_field($settings['shortcode']);
             $th_shortcode = do_shortcode( shortcode_unautop( $th_shortcode ) );
-            $this->add_render_attribute( 'th-form-class', 'class', esc_attr($settings['inline_form']) );
+
+            $th_form_border_class = false;
+            $th_formidable_class = 'th-form-default';
+            if ( isset($settings['inline_form']) && $settings['inline_form'] > "") :
+                switch ($settings['inline_form']) {
+                    case 'stacked':
+                        $th_formidable_class = 'th-form-stacked';
+                        if (isset($settings['slide_shortcode_border']) && $settings['slide_shortcode_border'] != 'none'){
+                            $th_form_border_class = $settings['slide_shortcode_border'];
+                        }
+                        break;
+                    case 'inline':
+                        $th_formidable_class = 'th-conversion';
+                        break;
+                }
+            endif;
+
+            $th_cal_align_class = false;
+            if(isset($settings['slide_text_align']) && $settings['slide_text_align'] > ""){
+                switch ($settings['slide_text_align']) {
+                    case 'left':
+                        $th_cal_align_class =  ' th-left';
+                        break;
+                    case 'center':
+                        $th_cal_align_class = ' th-centered';
+                        break;
+                    case 'right':
+                        $th_cal_align_class = ' th-right';
+                        break;
+                }
+            }
+
+            $this->add_render_attribute( 'th-form-class', 'class', 'th-fo-form');
+            $this->add_render_attribute( 'th-form-class', 'class', esc_attr($th_cal_align_class));
+            $this->add_render_attribute( 'th-form-class', 'class', esc_attr($th_formidable_class));
+            $this->add_render_attribute( 'th-form-class', 'class', esc_attr($th_form_border_class));
+
+
             ?>
             <div <?php echo $this->get_render_attribute_string( 'th-form-class'); ?>><?php echo $th_shortcode; ?></div>
             <?php
