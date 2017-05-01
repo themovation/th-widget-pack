88596b58f801fe01e45bddce93eb5da48a2121ac
ryanlabelle <ryan@honeycombweb.com>
2017-04-30 19:55:01 -0700

    Update icon for tours custom post type

--- inc/cpt_tours.php
+++ inc/cpt_tours.php
@@ -50,7 +50,7 @@ if ( ! function_exists('themo_tour_custom_post_type') ) {
             'show_in_nav_menus'   => true,
             'show_in_admin_bar'   => true,
             'menu_position'       => 5,
-            'menu_icon'           => 'dashicons-universal-access-alt',
+            'menu_icon'           => 'dashicons-location-alt',
             'can_export'          => true,
             'has_archive'         => false,
             'exclude_from_search' => false,
