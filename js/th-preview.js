74f2a6a80ec8d430285fe057f2f149d5b78d25d5
ryanlabelle <ryan@honeycombweb.com>
2017-10-13 12:48:19 -0700

    Add a small delay before jquery is run, making time for WP jquery to load.

--- js/th-preview.js
+++ js/th-preview.js
@@ -1,19 +1,33 @@
 /**
  * Created by rl on 2017-03-31.
  */
-jQuery( function( $ ) {
-    if ( undefined !== window.elementor ) {
-        console.log('We are in to the preview!');
-        /*
-          $( "header[data-transparent-header='true']:not('.headhesive--clone')" ).prepend( "<p class='hide-nav'><a href=''>Test</a></p>" );
-         */
 
-        $( "<div class='hide-nav-wrap button'><div class='hide-nav'>Hide/Show Header</div> </div>" ).insertAfter( "header[data-transparent-header='true']:not('.headhesive--clone')" );
 
-        $( ".hide-nav-wrap" ).click(function() {
-            $( "header[data-transparent-header='true']:not('.headhesive--clone')" ).fadeToggle( "fast", function() {
-                // Animation complete.
+
+(function() {
+    var runMyCode = function($) {
+        if ( undefined !== window.elementor ) {
+            console.log('We are in to the preview!');
+            /*
+             $( "header[data-transparent-header='true']:not('.headhesive--clone')" ).prepend( "<p class='hide-nav'><a href=''>Test</a></p>" );
+             */
+
+            $( "<div class='hide-nav-wrap button'><div class='hide-nav'>Hide/Show Header</div> </div>" ).insertAfter( "header[data-transparent-header='true']:not('.headhesive--clone')" );
+
+            $( ".hide-nav-wrap" ).click(function() {
+                $( "header[data-transparent-header='true']:not('.headhesive--clone')" ).fadeToggle( "fast", function() {
+                    // Animation complete.
+                });
             });
-        });
-    }
-} );
+        }
+    };
+
+    var timer = function() {
+        if (window.jQuery && window.jQuery.ui) {
+            runMyCode(window.jQuery);
+        } else {
+            window.setTimeout(timer, 100);
+        }
+    };
+    timer();
+})();
