a2d5559fd6ee1d3801364b3547390d3be65f9d81
ryanlabelle <ryan@honeycombweb.com>
2018-01-10 13:31:35 -0800

    Version bump.

--- th-widget-pack.php
+++ th-widget-pack.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Page Builder Widget Pack
- * Version: 1.0.4
+ * Version: 1.0.5
  * Plugin URI: themovation.com
  * Description: A widget pack for the Elementor Page Builder
  * Author: Themovation
