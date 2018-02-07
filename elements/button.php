d124377854b1bb39728217fe72bfa893ba143eeb
adiraomj <adiraomj@outlook.com>
2018-02-07 13:05:32 +0530

    Added translation for button and appointments

--- elements/button.php
+++ elements/button.php
@@ -328,6 +328,29 @@ class Themo_Widget_Button extends Widget_Base {
 		<# } #>
 		<?php
 	}
+
+	public function add_wpml_support() {
+		add_filter( 'wpml_elementor_widgets_to_translate', [ $this, 'wpml_widgets_to_translate_filter' ] );
+	}
+
+	public function wpml_widgets_to_translate_filter( $widgets ) {
+		$widgets[ $this->get_name() ] = [
+			'conditions' => [ 'widgetType' => $this->get_name() ],
+			'fields'     => [
+				[
+					'field'       => 'button_1_text',
+					'type'        => __( 'Button Text', 'th-widget-pack' ),
+					'editor_type' => 'LINE'
+				],
+				[
+					'field'       => 'button_2_text',
+					'type'        => __( 'Button Text', 'th-widget-pack' ),
+					'editor_type' => 'LINE'
+				],
+			],
+		];
+		return $widgets;
+	}
 }
 
 Plugin::instance()->widgets_manager->register_widget_type( new Themo_Widget_Button() );
--- languages/wpml-translations.php
+++ languages/wpml-translations.php
@@ -1,6 +1,8 @@
 <?php
 namespace Elementor;
 
+use Elementor\Themo_Widget_Appointments as Appointment;
+use Elementor\Themo_Widget_Button as Button;
 use Elementor\Themo_Widget_Formidable as Form;
 use Elementor\Themo_Widget_Itinerary as Itinerary;
 
@@ -17,21 +19,27 @@ class Themo_Elementor_Translate {
 	}
 
 	private function widgets_with_items() {
-		require THEMO_PATH . '/languages/wpml-itinerary.php';
+		//require THEMO_PATH . '/languages/wpml-itinerary.php';
 	}
 
 	private function includes() {
+		require_once THEMO_PATH . '/elements/appointments.php';
+		require_once THEMO_PATH . '/elements/button.php';
 		require_once THEMO_PATH . '/elements/formidable-form.php';
-		require_once THEMO_PATH . '/elements/itinerary.php';
+		//require_once THEMO_PATH . '/elements/itinerary.php';
 	}
 
 	public function add_wpml_support() {
 		$this->includes();
 		$this->widgets_with_items();
+		$appointments = new Appointment();
+		$appointments->add_wpml_support();
+		$button = new Button();
+		$button->add_wpml_support();
 		$form = new Form();
 		$form->add_wpml_support();
-		$itinerary = new Itinerary();
-		$itinerary->add_wpml_support();
+		// $itinerary = new Itinerary();
+		// $itinerary->add_wpml_support();
 	}
 }
 
