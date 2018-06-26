79978ede5f040dc96f4529c5b216e6e7d167f714
ryanlabelle <ryan@honeycombweb.com>
2018-06-25 18:08:21 -0700

    Reposition Widget Category for our custom widgets.

--- inc/elementor-section.php
+++ inc/elementor-section.php
@@ -1,5 +1,5 @@
 <?php
-namespace Elementor;
+//namespace Elementor;
 
 function themovation_elementor_section() {
 	Plugin::instance()->elements_manager->add_category(
@@ -11,4 +11,18 @@ function themovation_elementor_section() {
 		1
 	);
 }
-add_action( 'elementor/init', 'Elementor\themovation_elementor_section' );
+//add_action( 'elementor/init', 'Elementor\themovation_elementor_section' );
+
+
+function themo_add_elementor_widget_categories( $elements_manager ) {
+
+    $elements_manager->add_category(
+        'themo-elements',
+        [
+            'title' => __( 'Themovation Elements', 'th-widget-pack' ),
+            'icon' => 'font',
+        ]
+    );
+
+}
+add_action( 'elementor/elements/categories_registered', 'themo_add_elementor_widget_categories' );
