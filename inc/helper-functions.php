20faa1dcc484c9e50540ff09880fdd8512c8c1b5
ryanlabelle <ryan@honeycombweb.com>
2018-07-03 11:16:19 -0700

    Fix for Elementor 2.1,  Page Builder Widget pack using depreciated code.

--- inc/helper-functions.php
+++ inc/helper-functions.php
@@ -48,54 +48,33 @@ if ( ! function_exists( 'sync_ot_and_elem_page_settings' ) ) {
         if(isset($post->ID)){
             $post_id = $post->ID;
 
-            $page = \Elementor\PageSettings\Manager::get_page( $post_id );
+            $page = \Elementor\Plugin::$instance->documents->get( $post_id );
 
-            $elm_trans_header = $page->get_settings( 'themo_transparent_header' );
-            $elm_page_layout = $page->get_settings( 'themo_page_layout' );
-            $elm_hide_title = $page->get_settings( 'hide_title' );
-            $elm_header_content_style = $page->get_settings( 'themo_header_content_style' );
-            $elm_alt_logo = $page->get_settings( 'themo_alt_logo' );
+            if ( $page ) {
 
+                $elm_trans_header = $page->get_settings('themo_transparent_header');
+                $elm_page_layout = $page->get_settings('themo_page_layout');
+                $elm_hide_title = $page->get_settings('hide_title');
+                $elm_header_content_style = $page->get_settings('themo_header_content_style');
+                $elm_alt_logo = $page->get_settings('themo_alt_logo');
 
-
-            // Are there settings from the Elementor Page Options?
-            //$elm_page_settings = get_post_meta( $post_id,"_elementor_page_settings");
-
-            //var_dump($elm_page_settings[0]['section_page_settings']};
-
-            /*echo "<pre>";
-            va8r_dump($elm_page_settings[0]['themo_transparent_header']);
-            var_dump($elm_page_settings);
-            echo "</pre>";*/
-
-            /*if(isset($elm_page_settings[0]['themo_transparent_header']) && isset($elm_page_settings[0]['themo_page_layout'])
-                && isset($elm_page_settings[0]['hide_title']) && isset($elm_page_settings[0]['themo_header_content_style'])
-                && isset($elm_page_settings[0]['themo_alt_logo'])){*/
-
-                /*
-                 $elm_trans_header = $elm_page_settings[0]['themo_transparent_header'];
-                $elm_page_layout = $elm_page_settings[0]['themo_page_layout'];
-                $elm_hide_title = $elm_page_settings[0]['hide_title'];
-                $elm_header_content_style = $elm_page_settings[0]['themo_header_content_style'];
-                $elm_alt_logo = $elm_page_settings[0]['themo_alt_logo'];
-                */
-
-                if(empty($elm_trans_header)){
+                if (empty($elm_trans_header)) {
                     $elm_trans_header = 'off';
                 }
-                if(empty($elm_hide_title)){
+                if (empty($elm_hide_title)) {
                     $elm_hide_title = 'off';
-                }else{
+                } else {
                     $elm_hide_title = 'on';
                 }
 
-                update_post_meta($post_id,'themo_transparent_header',$elm_trans_header);
-                update_post_meta($post_id,'themo_page_layout',$elm_page_layout);
-                update_post_meta($post_id,'themo_hide_title',$elm_hide_title); //hide_title
-                update_post_meta($post_id,'themo_header_content_style',$elm_header_content_style);
-                update_post_meta($post_id,'themo_alt_logo',$elm_alt_logo);
+                update_post_meta($post_id, 'themo_transparent_header', $elm_trans_header);
+                update_post_meta($post_id, 'themo_page_layout', $elm_page_layout);
+                update_post_meta($post_id, 'themo_hide_title', $elm_hide_title); //hide_title
+                update_post_meta($post_id, 'themo_header_content_style', $elm_header_content_style);
+                update_post_meta($post_id, 'themo_alt_logo', $elm_alt_logo);
 
-            //}
+                //}
+            }
 
         }
     }
