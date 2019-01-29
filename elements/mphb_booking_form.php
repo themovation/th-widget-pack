4ce957016ae74125e3f0067e2ca759bf83cc6572
ryanlabelle <ryan@honeycombweb.com>
2019-01-29 12:14:44 -0800

    Adding form options: inline, stacked, theme options to enable / disable form styling.

--- elements/mphb_booking_form.php
+++ elements/mphb_booking_form.php
@@ -42,6 +42,84 @@ class Themo_Widget_MPHB_Booking_Form extends Widget_Base {
         ));
 
         $this->add_control(
+            'inline_form',
+            [
+                'label' => __( 'Formidable Form Style', 'th-widget-pack' ),
+                'type' => Controls_Manager::SELECT,
+                'default' => 'none',
+                'options' => [
+                    'none' => __( 'Default', 'th-widget-pack' ),
+                    'inline' => __( 'Inline', 'th-widget-pack' ),
+                    'stacked' => __( 'Stacked', 'th-widget-pack' ),
+
+                ],
+            ]
+        );
+
+        $this->add_control(
+            'slide_shortcode_border',
+            [
+                'label' => __( 'Form Background', 'th-widget-pack' ),
+                'type' => Controls_Manager::SELECT,
+                'default' => 'none',
+                'options' => [
+                    'none' => __( 'None', 'th-widget-pack' ),
+                    'th-form-bg th-light-bg' => __( 'Light', 'th-widget-pack' ),
+                    'th-form-bg th-dark-bg' => __( 'Dark', 'th-widget-pack' ),
+
+                ],
+                'condition' => [
+                    'inline_form' => 'stacked',
+                ],
+            ]
+        );
+
+        $this->add_control(
+            'button_1_style',
+            [
+                'label' => __( 'Button Style', 'th-widget-pack' ),
+                'type' => Controls_Manager::SELECT,
+                'default' => 'standard-primary',
+                'options' => [
+                    'standard-primary' => __( 'Standard Primary', 'th-widget-pack' ),
+                    'standard-accent' => __( 'Standard Accent', 'th-widget-pack' ),
+                    'standard-light' => __( 'Standard Light', 'th-widget-pack' ),
+                    'standard-dark' => __( 'Standard Dark', 'th-widget-pack' ),
+                    'ghost-primary' => __( 'Ghost Primary', 'th-widget-pack' ),
+                    'ghost-accent' => __( 'Ghost Accent', 'th-widget-pack' ),
+                    'ghost-light' => __( 'Ghost Light', 'th-widget-pack' ),
+                    'ghost-dark' => __( 'Ghost Dark', 'th-widget-pack' ),
+                    'cta-primary' => __( 'CTA Primary', 'th-widget-pack' ),
+                    'cta-accent' => __( 'CTA Accent', 'th-widget-pack' ),
+                ],
+            ]
+        );
+
+        $this->add_control(
+            'slide_text_align',
+            [
+                'label' => __( 'Align', 'th-widget-pack' ),
+                'type' => Controls_Manager::CHOOSE,
+                'label_block' => false,
+                'options' => [
+                    'left' => [
+                        'title' => __( 'Left', 'th-widget-pack' ),
+                        'icon' => 'fa fa-align-left',
+                    ],
+                    'center' => [
+                        'title' => __( 'Center', 'th-widget-pack' ),
+                        'icon' => 'fa fa-align-center',
+                    ],
+                    'right' => [
+                        'title' => __( 'Right', 'th-widget-pack' ),
+                        'icon' => 'fa fa-align-right',
+                    ],
+                ],
+                'default' => 'center',
+            ]
+        );
+
+        /*$this->add_control(
             'calendar_align',
             [
                 'label' => __( 'Center Align', 'th-widget-pack' ),
@@ -67,7 +145,7 @@ class Themo_Widget_MPHB_Booking_Form extends Widget_Base {
                     '{{WRAPPER}} .mphb_sc_booking_form-wrapper' => 'text-align: center;',
                 ],
             ]
-        );
+        );*/
 
 
         $this->add_control(
@@ -88,7 +166,7 @@ class Themo_Widget_MPHB_Booking_Form extends Widget_Base {
         $this->add_control(
             'content_max_width',
             [
-                'label' => __( 'Form Width', 'th-widget-pack' ),
+                'label' => __( 'Content Width', 'th-widget-pack' ),
                 'type' => Controls_Manager::SLIDER,
                 'range' => [
                     'px' => [
@@ -100,13 +178,9 @@ class Themo_Widget_MPHB_Booking_Form extends Widget_Base {
                         'max' => 100,
                     ],
                 ],
-                'default' => [
-                    'size' => '650',
-                    'unit' => 'px',
-                ],
                 'size_units' => [ '%', 'px' ],
                 'selectors' => [
-                    '{{WRAPPER}} .mphb_sc_booking_form-wrapper' => 'max-width: {{SIZE}}{{UNIT}};',
+                    '{{WRAPPER}} .th-fo-form' => 'max-width: {{SIZE}}{{UNIT}};',
                 ],
             ]
         );
@@ -136,6 +210,7 @@ class Themo_Widget_MPHB_Booking_Form extends Widget_Base {
                     'type' => Scheme_Color::get_type(),
                     'value' => Scheme_Color::COLOR_1,
                 ],
+
             ]
         );
 
@@ -145,6 +220,7 @@ class Themo_Widget_MPHB_Booking_Form extends Widget_Base {
                 'name' => 'tip_color_typography',
                 'selector' => '{{WRAPPER}} .mphb-required-fields-tip',
                 'scheme' => Scheme_Typography::TYPOGRAPHY_1,
+
             ]
         );
 
@@ -161,6 +237,7 @@ class Themo_Widget_MPHB_Booking_Form extends Widget_Base {
                     'type' => Scheme_Color::get_type(),
                     'value' => Scheme_Color::COLOR_1,
                 ],
+                'separator' => 'before',
             ]
         );
 
@@ -193,8 +270,46 @@ class Themo_Widget_MPHB_Booking_Form extends Widget_Base {
             $th_shortcode = sanitize_text_field( $th_shortcode );
             $th_shortcode = do_shortcode( shortcode_unautop( $th_shortcode ) );
 
+            $th_form_border_class = false;
+            $th_formidable_class = 'th-form-default';
+            if ( isset( $settings['inline_form'] ) && $settings['inline_form'] > "" ) :
+                switch ( $settings['inline_form'] ) {
+                    case 'stacked':
+                        $th_formidable_class = 'th-form-stacked';
+                        if ( isset( $settings['slide_shortcode_border'] ) && $settings['slide_shortcode_border'] != 'none' ) {
+                            $th_form_border_class = $settings['slide_shortcode_border'];
+                        }
+                        break;
+                    case 'inline':
+                        $th_formidable_class = 'th-conversion';
+                        break;
+                }
+            endif;
+
+            /* Form Styling */
+            $th_cal_align_class = false;
+            if ( isset( $settings['slide_text_align'] ) && $settings['slide_text_align'] > "" ) {
+                switch ( $settings['slide_text_align'] ) {
+                    case 'left':
+                        $th_cal_align_class = ' th-left';
+                        break;
+                    case 'center':
+                        $th_cal_align_class = ' th-centered';
+                        break;
+                    case 'right':
+                        $th_cal_align_class = ' th-right';
+                        break;
+                }
+            }
+
+            $this->add_render_attribute( 'th-form-class', 'class', 'th-fo-form');
+            $this->add_render_attribute( 'th-form-class', 'class', esc_attr( $th_cal_align_class ) );
+            $this->add_render_attribute( 'th-form-class', 'class', esc_attr( $th_formidable_class ) );
+            $this->add_render_attribute( 'th-form-class', 'class', esc_attr( $th_form_border_class ) );
+            $this->add_render_attribute( 'th-form-class', 'class', 'th-btn-form' );
+            $this->add_render_attribute( 'th-form-class', 'class', 'btn-' . esc_attr( $settings['button_1_style'] . '-form' ) );
             ?>
-            <div class="elementor-shortcode"><?php echo $th_shortcode; ?></div>
+            <div <?php echo $this->get_render_attribute_string( 'th-form-class'); ?>><?php echo $th_shortcode; ?></div>
             <?php
         }
     }
