b58a2de81652a538ef318af9ee5141ea59ea02ea
ryanlabelle <ryan@honeycombweb.com>
2019-03-04 11:10:09 -0800

    Provide tab to hide/show Groovy menu within Elementor.

--- js/th-preview.js
+++ js/th-preview.js
@@ -11,17 +11,42 @@
              $( "header[data-transparent-header='true']:not('.headhesive--clone')" ).prepend( "<p class='hide-nav'><a href=''>Test</a></p>" );
              */
 
-            $( "<div class='hide-nav-wrap button'><div class='hide-nav'>Hide/Show Header</div> </div>" ).insertAfter( "header[data-transparent-header='true']:not('.headhesive--clone')" );
+            /* Check if we are using Groovy Menu, if we are, check for css position of wrapper div */
 
-            $( ".hide-nav-wrap" ).click(function() {
-                $( "header[data-transparent-header='true']:not('.headhesive--clone')" ).fadeToggle( "fast", function() {
-                    // Animation complete.
+            if($( "header" ).hasClass( "gm-navbar" )){
+                console.log('Groovy Menu is active');
+                var themo_gm_menu_pos = $('header .gm-wrapper').css('position');
+                if(themo_gm_menu_pos == 'absolute'){
+                    console.log('Groovy menu position: '+themo_gm_menu_pos);
+
+                    $( "<div class='hide-nav-wrap button'><div class='hide-nav'>Hide/Show Header</div> </div>" ).insertAfter( "header.gm-navbar" );
+
+                    $( ".hide-nav-wrap" ).click(function() {
+                        $( "header.gm-navbar .gm-wrapper" ).fadeToggle( "fast", function() {
+                            // Animation complete.
+                        });
+                    });
+
+                    setTimeout(function() {
+                        $( ".hide-nav-wrap" ).trigger( "click" ); // Hide Nav on Start.
+                    }, 2000);
+                }
+            }else{
+                $( "<div class='hide-nav-wrap button'><div class='hide-nav'>Hide/Show Header</div> </div>" ).insertAfter( "header[data-transparent-header='true']:not('.headhesive--clone')" );
+
+                $( ".hide-nav-wrap" ).click(function() {
+                    $( "header[data-transparent-header='true']:not('.headhesive--clone')" ).fadeToggle( "fast", function() {
+                        // Animation complete.
+                    });
                 });
-            });
 
-            setTimeout(function() {
-                $( ".hide-nav-wrap" ).trigger( "click" ); // Hide Nav on Start.
-            }, 2000);
+                setTimeout(function() {
+                    $( ".hide-nav-wrap" ).trigger( "click" ); // Hide Nav on Start.
+                }, 2000);
+            }
+
+
+
 
 
         }
