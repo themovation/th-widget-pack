20fa4d5e4b0bfe80dac92f1cb332b0d15eb26b1d
Aumio <secretspeed.aumz@gmail.com>
2020-12-14 03:22:16 +0600

    Transparent Header fixed.

--- header-footer/inc/hfe-functions.php
+++ header-footer/inc/hfe-functions.php
@@ -82,8 +82,15 @@ function hfe_render_header() {
 		return;
 	}
 
+	$transparent_header = get_post_meta( get_hfe_header_id(), 'transparent-header', true );
+
+	$render_class = '';
+	if ( $transparent_header ) {
+		$render_class .= 'transparent-header';  
+	}
+
 	?>
-		<header id="masthead" itemscope="itemscope" itemtype="https://schema.org/WPHeader">
+		<header id="masthead" class="<?php echo $render_class; ?>" itemscope="itemscope" itemtype="https://schema.org/WPHeader">
 			<p class="main-title bhf-hidden" itemprop="headline"><a href="<?php echo bloginfo( 'url' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 			<?php Header_Footer_Elementor::get_header_content(); ?>
 		</header>
